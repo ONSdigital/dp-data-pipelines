f7b749374fb54e5ea1bb371605b91facb0355f96
mike <mikelovesbooks@gmail.com>
2024-02-05 13:38:01 +0000

    iniital structure

---
+++ Makefile
@@ -0,0 +1,1 @@
+# TODO - running tests. Conveniences for local dev (dependencies etc)
---
+++ pipeline/v1.py
@@ -0,0 +1,8 @@
+"""
+Version 1 of data ingress pipeline
+
+intended usage would be:
+
+import pipeline
+pipeline.v1(*args, **kwargs)
+"""
