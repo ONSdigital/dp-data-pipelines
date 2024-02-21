a5b5d14639a233ef10d317339e1d3ebfc5f98ba3
mike <mikelovesbooks@gmail.com>
2024-02-21 18:41:04 +0000

    reorganised, tweaks, add Makefile, ran black

--- pipelines/pipeline/shared/pipelineconfig/matching.py
+++ pipelines/pipeline/shared/pipelineconfig/matching.py
@@ -1,5 +1,5 @@
 """This script will contain functions for the pipeline"""
-from multiprocessing.sharedctypes import Value
+
 from typing import List
 
 
@@ -7,15 +7,17 @@ def get_supplementary_distribution_patterns(config: dict) -> List[str]:
     """
     Given a pipeline config in the form of a dictionary, return
     a list of regex patterns for finding supplementary distributions.
-    """
-    expected_id_value = "https://raw.githubusercontent.com/ONSdigital/sandbox/initial-structure/schemas/ingress/config/v1.json"
 
+    Raise an exception where the pipeline config is not one we have
+    handling for.
+    """
     list_of_matches = []
-    if config["$id"] != expected_id_value:
-        raise ValueError(f"the `$id` value is missing or incorrect! It should be {expected_id_value}")
-    else:
+    if config["$id"].endswith("initial-structure/schemas/ingress/config/v1.json"):
         for x in config["supplementary_distributions"]:
             value = x["matches"]
             list_of_matches.append(value)
-
         return list_of_matches
+    else:
+        raise ValueError(
+            f"No supplementary distribution handling for config {config['$id']}"
+        )
