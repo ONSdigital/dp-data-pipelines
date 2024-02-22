f2d01bb3747f3f7a5eafbe0c84e299040a110740
James Bryant <jamesbryant792@gmail.com>
2024-02-22 09:24:27 +0000

    updated tests

--- tests/pipelines/pipeline/functions/test_config.py
+++ tests/pipelines/pipeline/functions/test_config.py
@@ -1,35 +1,34 @@
 import pytest
-import sys
 
-sys.path.append("../../../..") # path to dp-data-pipelines folder
 from pipelines.pipeline.functions.config import get_pipeline_identifier_from_config
 
 def test_get_pipeline_identifier_from_config():
     config_dict = {
             "$id": "https://raw.githubusercontent.com/ONSdigital/dp-data-pipelines/sandbox/schemas/dataset-ingress/config/v1.json",
-            "pipeline": "pipeline-dataset-ingress-v1.yml"
+            "pipeline": "dataset_ingress_v1.yml"
         }
-    get_pipeline_identifier_from_config(config_dict)
+    pipeline = get_pipeline_identifier_from_config(config_dict)
+    assert pipeline == "dataset_ingress_v1.yml"
     
 
 def test_get_pipeline_identifier_from_config_incorrect_schema_id():
     config_dict = {
             "$id": "https://raw.githubusercontent.com/ONSdigital/dp-data-pipelines/sandbox/schemas/dataset-ingress/config/v2.json",
-            "pipeline": "pipeline-dataset-ingress-v1.yml"
+            "pipeline": "dataset_ingress_v1.yml"
         }
     with pytest.raises(NotImplementedError) as err:
         get_pipeline_identifier_from_config(config_dict)
         
-    assert str(err.value) == "Only currently built to accept '/schemas/dataset-ingress/config/v1.json' as valid $id"
-
+    assert str(err.value) == f"{config_dict['$id']} is not a recognised $id"
+    
 
 def test_get_pipeline_identifier_from_config_incorrect_dict():
     config_dict = {
             "$id": "https://raw.githubusercontent.com/ONSdigital/dp-data-pipelines/sandbox/schemas/dataset-ingress/config/v1.json"
         }
-    with pytest.raises(NotImplementedError) as err:
+    with pytest.raises(AssertionError) as err:
         get_pipeline_identifier_from_config(config_dict)
         
-    assert str(err.value) == "Current acceptable formats require 'pipeline' to be in the top level of the config dict"
+    assert 'pipeline is not in the config dict' in str(err.value)
 
 
