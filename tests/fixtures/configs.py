2bee0ac8eab5c19ca8d00ec88690cbe80b5c5187
mike <mikelovesbooks@gmail.com>
2024-02-22 11:29:35 +0000

    tidy up

--- tests/fixtures/configs.py
+++ tests/fixtures/configs.py
@@ -4,18 +4,18 @@ from pathlib import Path
 import pytest
 
 this_dir = Path(__file__).parent
-text_cases_dir = Path(this_dir / "test-cases")
+test_cases_dir = Path(this_dir / "test-cases")
 
 
 @pytest.fixture
 def config_unknown_id():
-    with open(text_cases_dir / "test-pipeline-config-unknown-id.json") as f:
+    with open(test_cases_dir / "test-pipeline-config-unknown-id.json") as f:
         config = json.load(f)
     return config
 
 
 @pytest.fixture
 def config_valid_multiple_supplementary_distributions():
-    with open(text_cases_dir / "test-pipeline-config-multiple-supplementary-distributions.json") as f:
+    with open(test_cases_dir / "test-pipeline-config-multiple-supplementary-distributions.json") as f:
         config = json.load(f)
     return config
