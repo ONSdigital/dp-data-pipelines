78d86fd7dd153f8b1b3328e76a7461f1411d3ac1
bailed22 <david.bailey@ons.gov.uk>
2024-02-27 09:21:33 +0000

    correction to variables

--- pipeline/shared/functons/notification.py
+++ pipeline/shared/functons/notification.py
@@ -1,47 +1,59 @@
 import json
 import os
-#from dpytools.http_clients.base import BaseHttpClient
-
-
-from pipeline.shared.functons.slack import SlackNotifier
+from dpytools.slack.slack import SlackMessenger
 
 #slack_webhook_env_var = os.environ["SLACK_WEBHOOK"]
+# Create client for messaging publishing support
+PS_SLACK_WEBHOOK = os.environ.get("PS_SLACK_WEBHOOK", None)
+assert PS_SLACK_WEBHOOK is not None, "Unable to find required environment variable PS_SLACK_WEBHOOK"
+ps_slack_client = SlackMessenger(PS_SLACK_WEBHOOK)
 
-def publishing_support(publishing_webhook_url: str, msg: str):
+def publishing_support(msg: str):
     """
     Send a messge to a channel or avanue of contact such that it reaches the
     publishing support team.
     """
-    publishing_notifier = SlackNotifier(publishing_webhook_url)
-    publishing_notifier.msg_str(msg)
+    ps_slack_client.msg_str(msg)
 
-def data_engineering(data_eng_webhook_url: str, msg: str):
+
+DE_SLACK_WEBHOOK = os.environ.get("DE_SLACK_WEBHOOK", None)
+assert DE_SLACK_WEBHOOK is not None, "Unable to find required environment variable DE_SLACK_WEBHOOK"
+de_slack_client = SlackMessenger(DE_SLACK_WEBHOOK)
+
+def data_engineering(msg: str):
     """
     Send a messge to a channel or avanue of contact such that it reaches the
     data engineers.
     """
-    data_engineering_notifier = SlackNotifier(data_eng_webhook_url)
-    data_engineering_notifier.msg_str(msg)
+    de_slack_client.msg_str(msg)
+
 
 # IMPORTANT - where we're notifying the SEs we also want to notify the DEs so they
 # know there's a software issues that's being looked into.
-def software_engineering(software_eng_webhook_url, msg):
+SE_SLACK_WEBHOOK = os.environ.get("SE_SLACK_WEBHOOK", None)
+assert SE_SLACK_WEBHOOK is not None, "Unable to find required environment variable SE_SLACK_WEBHOOK"
+se_slack_client = SlackMessenger(SE_SLACK_WEBHOOK)   
+
+def software_engineering(msg: str):
     """
     Send a messge to a channel or avaenue of contact such that it reaches the
     software engineers.
     """
     #software engineer slack notifier
     #software engineer message code
-    software_engineering_notifier = SlackNotifier(software_eng_webhook_url)
-    software_engineering_notifier.msg_str(msg)
+    de_slack_client.msg_str(msg)
+
 
 # IMPORTANT - where we're notifying the submitter we also want to notify the DEs so they
 # know there's a submission issue that's being looked into.
-def data_submmitter_of_validation_error(data_submitter_webhook_url, msg):
+DS_SLACK_WEBHOOK = os.environ.get("DS_SLACK_WEBHOOK", None)
+assert DS_SLACK_WEBHOOK is not None, "Unable to find required environment variable DS_SLACK_WEBHOOK"
+ds_slack_client = SlackMessenger(DS_SLACK_WEBHOOK) 
+
+def data_submmitter_of_validation_error(msg: str):
     """
     Send a messge to a channel or avaenue of contact such that it reaches the
     registred contact email the data was submitted with. Include information on
     a specific json schema validation error.
     """
-    data_submmitter_of_validation_error_notifier = SlackNotifier(data_submitter_webhook_url)
-    data_submmitter_of_validation_error_notifier.msg_str(msg)
+    ds_slack_client.msg_str(msg)
