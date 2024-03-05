2634d9ea650b4d79d900bf7795d6736c995b85af
Moasib-Arif <moasib_arif@yahoo.com>
2024-03-05 12:22:51 +0000

    Working

--- README.md
+++ README.md
@@ -4,34 +4,20 @@
 
 Pipeline specific python scripts and tooling for automated website data ingress.
 
-There are two things to understand here:
+## Installation
+This repository is provided as an installable python package
 
-- Architecture
-- Implementation
+1. **Install the package**
 
-We'll take each in turn then discuss repository structure.
+    Open your terminal and run the following command:
 
+    ```bash
+    pip install git+https://github.com/ONSdigital/dp-data-pipelines.git
+    ```
 
-### Architecture
+## Usage
 
-The following diagram shows a simple overview of how this repository is used to manage variants of simple data pipelines for the purpose of shunting data assets from outside the websites services to within - via submissions to an aws s3 bucket.
-
-Note - This is a high level diagram only of _how_ we run pipeline**s** (as in, the usage of this github repo and how it interacts with infrastructure) to contextualise the code present rather than a strict architectural diagram.
-
-_How_ the pipelines mentioned below actually work is covered in the following _Implementation_ section.
-
-![Overview](./docs/overview.png)
-
-
-### Implementation
-
-The initial example of this system works as follows:
-
-- The lambda triggers the buildspec [./s3_tar_received.yml](./s3_tar_received.yml).
-- This triggers [./pipelines/s3_tar_received.py](./pipelines/s3_tar_received.py) - this function triggers a pipeline from `./pipelines/pipeline` based on the [pipeline-config.json](./docs/pipeline-config.md) present in the tar file.
-- **In our initial use case** it triggers our intial pipeline [dataset_ingress_v1.py](./pipelines/pipeline/dataset_ingress_v1.py).
-
-The principle here is that the the details in pipeline config can be used to nuance which pipeline from `./pipelines/pipeline` is called as well as the options that are passed to it.
+TODO: Add usage instructions here.
 
 ## Repository Structure
 
@@ -51,3 +37,10 @@ The following is the (initially, it'll expand over time) structure of this repos
      - s3_tar_received.yml                # Calls ./pipelines/s3_tar_recieved.py. Triggered by lambda
      - builder.yml                        # Rebuilds /builder/Dockerfile on changes to that directory
 ```
+
+Licence
+-------
+
+Copyright ©‎ 2024, Office for National Statistics (https://www.ons.gov.uk)
+
+Released under MIT license, see [LICENSE](LICENSE) for details.
--- tests/pipelines/pipeline/shared/test_notification.py
+++ tests/pipelines/pipeline/shared/test_notification.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_notify_se_webook_raises_for_missing_env_var():
+def test_notify_se_webook_raises_for_missing_env_var(monkeypatch):
     """
     Test that when we import notification, an assertion error
     is correctly raised if we're missing the env var
@@ -9,17 +9,16 @@ def test_notify_se_webook_raises_for_missing_env_var():
 
     # use monkeypatch to set all the env vars other than
     # the one you're testing for the absense of
-    mp = pytest.MonkeyPatch()
-    mp.setenv("PS_SLACK_WEBHOOK", "not-used")
-    mp.setenv("DE_SLACK_WEBHOOK", "not-used")
-    mp.setenv("DS_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("PS_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("DE_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("DS_SLACK_WEBHOOK", "not-used")
 
     with pytest.raises(AssertionError) as e:
         from dpypelines.pipeline.shared import notification
 
-    assert "Unable to find required environment variable SE_SLACK_WEBHOOK" in str(e)
+    assert "Unable to find required environment variable SE_SLACK_WEBHOOK" in str(e.value)
 
-def test_notify_ps_webook_raises_for_missing_env_var():
+def test_notify_ps_webook_raises_for_missing_env_var(monkeypatch):
     """
     Test that when we import notification, an assertion error
     is correctly raised if we're missing the env var
@@ -28,17 +27,16 @@ def test_notify_ps_webook_raises_for_missing_env_var():
 
     # use monkeypatch to set all the env vars other than
     # the one you're testing for the absense of
-    mp = pytest.MonkeyPatch()
-    mp.setenv("SE_SLACK_WEBHOOK", "not-used")
-    mp.setenv("DE_SLACK_WEBHOOK", "not-used")
-    mp.setenv("DS_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("SE_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("DE_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("DS_SLACK_WEBHOOK", "not-used")
 
     with pytest.raises(AssertionError) as e:
-        from pipelines.pipeline.shared import notification
+        from dpypelines.pipeline.shared import notification
 
-    assert "Unable to find required environment variable PS_SLACK_WEBHOOK" in str(e)  
+    assert "Unable to find required environment variable PS_SLACK_WEBHOOK" in str(e.value)
 
-def test_notify_de_webook_raises_for_missing_env_var():
+def test_notify_de_webook_raises_for_missing_env_var(monkeypatch):
     """
     Test that when we import notification, an assertion error
     is correctly raised if we're missing the env var
@@ -47,17 +45,16 @@ def test_notify_de_webook_raises_for_missing_env_var():
 
     # use monkeypatch to set all the env vars other than
     # the one you're testing for the absense of
-    mp = pytest.MonkeyPatch()
-    mp.setenv("PS_SLACK_WEBHOOK", "not-used")
-    mp.setenv("SE_SLACK_WEBHOOK", "not-used")
-    mp.setenv("DS_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("PS_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("SE_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("DS_SLACK_WEBHOOK", "not-used")
 
     with pytest.raises(AssertionError) as e:
         from dpypelines.pipeline.shared import notification
 
-    assert "Unable to find required environment variable DE_SLACK_WEBHOOK" in str(e) 
+    assert "Unable to find required environment variable DE_SLACK_WEBHOOK" in str(e.value)
 
-def test_notify_ds_webook_raises_for_missing_env_var():
+def test_notify_ds_webook_raises_for_missing_env_var(monkeypatch):
     """
     Test that when we import notification, an assertion error
     is correctly raised if we're missing the env var
@@ -66,12 +63,11 @@ def test_notify_ds_webook_raises_for_missing_env_var():
 
     # use monkeypatch to set all the env vars other than
     # the one you're testing for the absense of
-    mp = pytest.MonkeyPatch()
-    mp.setenv("PS_SLACK_WEBHOOK", "not-used")
-    mp.setenv("DE_SLACK_WEBHOOK", "not-used")
-    mp.setenv("SE_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("PS_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("DE_SLACK_WEBHOOK", "not-used")
+    monkeypatch.setenv("SE_SLACK_WEBHOOK", "not-used")
 
     with pytest.raises(AssertionError) as e:
         from dpypelines.pipeline.shared import notification
 
-    assert "Unable to find required environment variable DS_SLACK_WEBHOOK" in str(e)         
+    assert "Unable to find required environment variable DS_SLACK_WEBHOOK" in str(e.value)
