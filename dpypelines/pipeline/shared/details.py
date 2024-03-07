f4920ef1cc733d832907ecf821fcbbb7f1f2c246
mike <mikelovesbooks@gmail.com>
2024-03-07 16:17:55 +0000

    minor tidy up

--- dpypelines/pipeline/shared/details.py
+++ dpypelines/pipeline/shared/details.py
@@ -1,11 +1,9 @@
-from transforms.sdmx.v1 import smdx_default_v1, sdmx_sanity_check_v1
+from transforms.sdmx.v1 import sdmx_sanity_check_v1, smdx_default_v1
 
 all_pipeline_details = {
-	"sdmx.default": {
-		"transform": smdx_default_v1,
-		"transform_inputs": {
-			"*.sdmx": sdmx_sanity_check_v1
-		},
-		"transform_kwargs": {}
+    "sdmx.default": {
+        "transform": smdx_default_v1,
+        "transform_inputs": {"*.sdmx": sdmx_sanity_check_v1},
+        "transform_kwargs": {},
     }
 }
