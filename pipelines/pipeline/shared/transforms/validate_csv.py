e7dd35e6d96656421a4c10b55d869f533f3ff316
James Bryant <jamesbryant792@gmail.com>
2024-03-11 17:09:10 +0000

    re-worked functions

--- pipelines/pipeline/shared/transforms/validate_csv.py
+++ pipelines/pipeline/shared/transforms/validate_csv.py
@@ -1,52 +1,29 @@
-# these validations are based off of a limited knowledge of what the final csv will look like and contain
-# seems likely that more validation steps will want to be added, extra steps can be added easily into
-# the validate_csv() function. If the new validation step checks columns one at a time then add inside
-# of the loop within validate_csv
+# this is a "functions that validate dataframes" toolbox
+# will be pieced together in another ticket (delete this text when it gets picked up)
+# more functions can be added as needed
 
 
-from typing import List, Any
+from typing import List
 from pathlib import Path
 import pandas as pd
-import json
 
-def _read_in_csv(csv):
+def _read_in_csv(csv_path):
     """
     given a csv will read in first 5 lines as a check
     """
+    assert csv_path.name.endswith('.csv'), "Invalid csv_path"
+    
     try:
-        df_sample = pd.read_csv(csv, nrows=5)
+        pd.read_csv(csv_path, nrows=5)
     except:
-        raise Exception(f"Failed to read in csv - {csv}")
+        raise Exception(f"Failed to read in csv - {csv_path}")
         
         
-def _read_in_metadata(metadata):
+def _correct_columns_exist(csv_path, columns: List[str]):
     """
-    given a metadata json file attempts to read in a metadata json file
-    returns the metadata as a dict if successful
+    given a list of columns, make sure those columns are in the dataframe
     """
-    try:
-        with open(metadata, 'r') as f:
-            metadata_json = json.load(f)
-        return metadata_json
-    except:
-        raise Exception(f"Failed to read in metadata - {metadata}")
-        
-
-def _check_metadata_columns(metadata_json):
-    """
-    given a metadata json file checks that the column data is where it is expected to be
-    """
-    try:
-        metadata_json['editions'][0]['table_schema']['columns']
-    except:
-        raise KeyError("['editions'][0]['table_schema']['columns'] not found in metadata file")
-
-
-def _correct_columns_exist(csv, columns: List[str]):
-    """
-    given a list of columns, make sure those columns are in the csv
-    """
-    df = pd.read_csv(csv, nrows=0) # reading in columns only
+    df = pd.read_csv(csv_path, nrows=0) # reading in columns only
     
     assert len(df.columns) == len(columns), f"Number of columns in csv file ({len(df.columns)}) does not match len of expected columns ({len(columns)})"
     
@@ -57,94 +34,62 @@ def _correct_columns_exist(csv, columns: List[str]):
     # need to check the other way around
 
 
-def _column_has_type(df, column: str, data_type: Any):
-    """
-    given a df, a column name and a type, make sure that all
-    values in that column can be case to that type (int, float, str etc)
-    """
-    # just a dict to tody up any spelling discrepancies
-    type_dict = {
-            'string': 'str',
-            'bool': 'bool',
-            'int': 'int',
-            'integer': 'int',
-            'float': 'float'
-        }
-    
-    expected_data_type = type_dict.get(data_type.lower(), None) # expected value is from the metadata
-    
-    if expected_data_type == None:
-        raise NotImplementedError(f"Currently have no conversion for data_type {data_type} given from metadata")
-    
-    observed_data_type = df[column].dtypes # observed value is from the dataframe 
-    
-    # pandas dataframes store dtypes as numpy dtypes so cannot be directly compared 
-    # with regular python dtypes
-    # this is checked by finding out if the expected dtype word is found in dataframe dtype word
-    # ie is 'int' found in 'numpy.dtypeint64'
-    # strings work slightly differently
-    if expected_data_type != 'str':
-        assert expected_data_type in str(observed_data_type), f"Expected data type for column {column} is given as {data_type} but observed data type from df is {observed_data_type}"
-    
-    else:
-        # pandas df usually stores strings as objects dtype, object dtype also covers multi types
-        # so will need to check each value individually
-        for value in df[column].unique():
-            assert type(value) == str, f"{value} in {column} column should be of type str not {type(value)}"
-
-
-def _column_has_no_blanks(df, column: str):
+def _dataframe_has_no_blanks(df):
     """
     given a df and a column name confirm that there are
     no blank entries in the column.
     """
-    # will treat '' & pd.isnull as blanks                 
-    for value in df[column].unique():
-        if type(value) == bool:
-            # bool can only be True of False so not empty
-            continue
-        elif type(value) == str:
-            if not value.strip(): # catches whitespace strings
-                raise ValueError(f"{column} has blank entries - '{value}' found")
-        else:
-            if pd.isnull(value):
-                raise ValueError(f"{column} has blank entries - '{value}' found")
+    for col in df.columns:
+        # will treat '' & pd.isnull as blanks                 
+        for value in df[col].unique():
+            if type(value) == bool:
+                # bool can only be True of False so not empty
+                continue
+            elif type(value) == str:
+                if not value.strip(): # catches whitespace strings
+                    raise ValueError(f"{col} has blank entries - '{value}' found")
+            else:
+                if pd.isnull(value):
+                    raise ValueError(f"{col} has blank entries - '{value}' found")
+                    
+
+def _dataframe_has_no_duplicates(df):
+    """
+    given a df confirm that there are no duplicate values
+    """
+    assert len(df) == len(df.drop_duplicates()), "Found duplicate rows in the dataframe, failed validation"
+    
+
+def dataframing_slicing(csv_path):
+    """
+    given a csv path read in the dataframe as slices and return these slices as a list
+    """
+    # currently a generic slice size, theres probably a most efficient size
+    chunk_size = 5000 
+    dataframe_slice_list = []
+
+    for df_slice in pd.read_csv(csv_path, chunksize=chunk_size):
+        dataframe_slice_list.append(df_slice)
+
+    return dataframe_slice_list
 
 
-def validate_csv(csv_path: Path, metadata_path: Path):
+def validate_csv(csv_path: Path, columns: List[str]):
     """
     Validate a csv
     """
     
-    # check if csv_path is a csv
-    assert csv_path.name.endswith('.csv'), "Invalid csv_path"
-    
-    # check if metadata_path is a json file
-    assert metadata_path.name.endswith('.json'), "Invalid metadata_path"
-    
-    # check that csv_path can be read in
-    _read_in_csv(csv_path)
-        
-    # same with metadata_path but will read in whole file and keep
-    metadata_json = _read_in_metadata(metadata_path)
-    
-    # column metadata found in expected place in dict
-    _check_metadata_columns(metadata_json)
-        
-    # checking column are as expected 
-    expected_columns = {}
-    for col in metadata_json['editions'][0]['table_schema']['columns']:
-        expected_columns.update({col['name']: col['datatype']})
-        
-    _correct_columns_exist(csv_path, list(expected_columns))
-    
-    # validating each column
-    # will be reading in column data here to avoid reading in twice per column
-    for col in expected_columns:
-        df_column = pd.read_csv(csv_path, usecols=[col]) # df with one column
-        # checking no blanks before dtype because if a column has an empty value
-        # it can change the dtype of the whole column
-        _column_has_no_blanks(df_column, col)
-        _column_has_type(df_column, col, expected_columns[col])
-        
+    # this function could look something like the below
 
+    # check that csv_path can be read in
+    # _read_in_csv(csv_path)
+    
+    # check correct columns in data
+    # _dataframe_correct_columns_exist(csv_path, columns)
+    
+    # get the dataframe as slices
+    # df_slice_list = dataframing_slicing(csv_path)
+    # for df_slice in df_slice_list:
+    #   _dataframe_has_no_duplicates(df_slice)
+    #   _dataframe_column_has_no_blanks(df_slice)
+        
--- tests/pipelines/pipeline/shared/transforms/test_validate_csv.py
+++ tests/pipelines/pipeline/shared/transforms/test_validate_csv.py
@@ -5,34 +5,22 @@ import pandas as pd
 
 from pipelines.pipeline.shared.transforms.validate_csv import (
     _read_in_csv,
-    _read_in_metadata,
-    _check_metadata_columns,
     _correct_columns_exist,
-    _column_has_type,
-    _column_has_no_blanks,
-    validate_csv
+    _dataframe_has_no_blanks,
+    _dataframe_has_no_duplicates,
+    dataframing_slicing,
 )
 
 test_dir = Path(__file__).parents[4]
 fixtures_files_dir = Path(test_dir / "fixtures/test-cases")
 
-def test_validate_csv_invalid_csv_path():
+def test_read_in_csv_invalid_csv_path():
     csv_path = Path('')
-    metadata_path = Path('')
     
     with pytest.raises(AssertionError) as err:
-        validate_csv(csv_path, metadata_path)
+        _read_in_csv(csv_path)
         
     assert "Invalid csv_path" in str(err.value)
-
-def test_validate_csv_invalid_matadata_path():
-    csv_path = Path('data.csv')
-    metadata_path = Path('')
-    
-    with pytest.raises(AssertionError) as err:
-        validate_csv(csv_path, metadata_path)
-        
-    assert "Invalid metadata_path" in str(err.value)
 
 def test_validate_csv_invalid_file():
     csv_path = Path('data.csv')
@@ -41,22 +29,6 @@ def test_validate_csv_invalid_file():
         _read_in_csv(csv_path)
         
     assert str(err.value) == f"Failed to read in csv - {csv_path}"
-    
-def test_validate_metadata_invalid_file():
-    metadata_path = Path('metadata.json')
-    
-    with pytest.raises(Exception) as err:
-        _read_in_metadata(metadata_path)
-        
-    assert str(err.value) == f"Failed to read in metadata - {metadata_path}"
-
-def test_check_metadata_columns_invalid_format():
-    metadata_json = {'key': 'value'}
-    
-    with pytest.raises(KeyError) as err:
-        _check_metadata_columns(metadata_json)
-        
-    assert "['editions'][0]['table_schema']['columns'] not found in metadata file" in str(err.value)
 
 def test_correct_columns_exist_incorrect_number_of_columns():
     fixture_file = Path(fixtures_files_dir / 'test_validate_csv_data.csv')
@@ -78,63 +50,42 @@ def test_correct_columns_exist_different_column_name():
         
     assert f"Expect column {incorrect_column} not found in csv file" in str(err.value)
     
-def test_column_has_type_unexpected_data_type():
-    df = pd.DataFrame({'col1': ['a', 'b'], 'col2': ['c', 'd']})
-    column = 'col1'
-    data_type = 'boolean'
-    
-    with pytest.raises(NotImplementedError) as err:
-        _column_has_type(df, column, data_type)
-        
-    assert str(err.value) == f"Currently have no conversion for data_type {data_type} given from metadata"
-    
-def test_column_has_type_unmatching_data_types():
-    df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-    column = 'col1'
-    data_type = 'bool'
-    observed_data_type = 'int64'
-    
-    with pytest.raises(AssertionError) as err:
-        _column_has_type(df, column, data_type)
-        
-    assert f"Expected data type for column {column} is given as {data_type} but observed data type from df is {observed_data_type}" in str(err.value)
-
-def test_column_has_type_incorrect_str_value():
-    df = pd.DataFrame({'col1': ['1', True], 'col2': ['3', '4']})
-    column = 'col1'
-    data_type = 'string'
-    value = True
-    
-    with pytest.raises(AssertionError) as err:
-        _column_has_type(df, column, data_type)
-        
-    assert f"{value} in {column} column should be of type str not {type(value)}" in str(err.value)
-
-def test_column_has_no_blanks_whitespace_entry():
+def test_dataframe_has_no_blanks_whitespace_entry():
     blank_value = '     '
     df = pd.DataFrame({'col1': ['1', blank_value], 'col2': ['3', '4']})
     column = 'col1'
     
     with pytest.raises(ValueError) as err:
-        _column_has_no_blanks(df, column)
+        _dataframe_has_no_blanks(df)
         
     assert str(err.value) == f"{column} has blank entries - '{blank_value}' found"
 
-def test_column_has_no_blanks_none_entry():
+def test_dataframe_has_no_blanks_none_entry():
     blank_value = None
     df = pd.DataFrame({'col1': ['1', blank_value], 'col2': ['3', '4']})
     column = 'col1'
     
     with pytest.raises(ValueError) as err:
-        _column_has_no_blanks(df, column)
+        _dataframe_has_no_blanks(df)
         
     assert str(err.value) == f"{column} has blank entries - '{blank_value}' found"
 
-def test_validate_csv():
-    fixture_csv_file = Path(fixtures_files_dir / 'test_validate_csv_data.csv')
-    fixture_metadata_file = Path(fixtures_files_dir / 'test_validate_csv_metadata.json')
+def test_dataframe_has_no_duplicates():
+    df = pd.DataFrame({'col1': ['1', '2', '1'], 'col2': ['3', '4', '3']}) # rows 1 & 3 the same
+    
+    with pytest.raises(AssertionError) as err:
+        _dataframe_has_no_duplicates(df)
+        
+    assert "Found duplicate rows in the dataframe, failed validation" in str(err.value)
+
+def test_dataframing_slicing():
+    fixture_file = Path(fixtures_files_dir / 'test_validate_csv_data.csv')
     
     try:
-        validate_csv(fixture_csv_file, fixture_metadata_file)
+        dataframe_list = dataframing_slicing(fixture_file)
     except:
         raise Exception('Unexpected error')
+        
+    assert type(dataframe_list) == list
+    assert type(dataframe_list[0]) == pd.core.frame.DataFrame
+    
