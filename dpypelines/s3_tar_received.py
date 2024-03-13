98501adae66464fdfa10fd410c093e4f831a8545
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-03-13 09:26:45 +0000

    made changes

--- dpypelines/s3_tar_received.py
+++ dpypelines/s3_tar_received.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import os
 
 from dpytools.stores.directory.local import LocalDirectoryStore
 from dpypelines.pipeline.shared import notification as notify
@@ -20,10 +20,27 @@ def start(s3_object_name: str):
 
         # decompress tar file to workspace
         decompress_s3_tar(s3_object_name, "input")
-        localStore = LocalDirectoryStore("input")
     except Exception as error:
         notify.data_engineering("ERROR MESSAGE")
         raise error
+    
+        # Identify the child directory we've just decompressed to within /inputs
+    try:
+        directories = [d for d in os.listdir("inputs") if os.path.isdir(os.path.join("inputs", d))]
+        assert len(directories) == 1, (
+            "Aborting, input directory has more than one directory within it"
+        )
+        decompressed_directory = directories[0]
+    except Exception as err:
+        notify.data_engineering("MESSAGE")
+        raise err
+
+        # Create a local directory store using our new files
+    try:
+        localStore = LocalDirectoryStore(f"inputs/{decompressed_directory}")
+    except Exception as err:
+        notify.data_engineering("MESSAGE")
+        raise err
 
         # confirm we have a config
     try:
@@ -39,13 +56,20 @@ def start(s3_object_name: str):
         # use config schema to confirm that config is valid
     try:
         config_dict = localStore.get_lone_matching_json_as_dict("pipeline-config.json")
+    except Exception as error:
+        notify.data_engineering("ERROR MESSAGE")
+        raise error
+    try:
         path_to_schema = get_config_schema_path(config_dict)
+    except Exception as error:
+        notify.data_engineering("ERROR MESSAGE")
+        raise error
+        
+    try:    
         validation.validate_json_schema(schema_path=path_to_schema, data_dict=config_dict)
     except Exception as error:
         notify.data_engineering("ERROR MESSAGE")
         raise error
-
-    
         # use the config info to select and run the appropriate pipeline,
 
         # there should literally by a field in the pipeline-config telling
