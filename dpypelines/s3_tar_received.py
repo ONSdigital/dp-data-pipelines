398a88789fb8766e4bda3aef6b439885caf1e905
mike <mikelovesbooks@gmail.com>
2024-03-13 07:53:56 +0000

    more trial code

--- dpypelines/s3_tar_received.py
+++ dpypelines/s3_tar_received.py
@@ -1,9 +1,10 @@
-from dpytools.s3.basic import decompress_s3_tar
 import os
 
+from dpytools.s3.basic import decompress_s3_tar
 from dpytools.stores.directory.local import LocalDirectoryStore
 
 from dpypelines.pipeline.shared import notification as notify
+from dpypelines.pipeline.shared import message
 
 def start(s3_object_name: str):
     """
@@ -16,8 +17,11 @@ def start(s3_object_name: str):
     # Decompress the tar to ./inputs
     try:
         decompress_s3_tar(s3_object_name, "inputs")
+        notify.data_engineering(f"Received s3 submission: s3://{s3_object_name}")
     except Exception as err:
-        notify.data_engineering("MESSAGE")
+        notify.data_engineering(
+            message.unexpected_error(f"Unable to decompress s3 object: {s3_object_name}", err)
+            )
         raise err
 
     # Identify the child directory we've just decompressed to within /inputs
@@ -27,6 +31,7 @@ def start(s3_object_name: str):
             "Aborting, input directory has more than one directory within it"
         )
         decompressed_directory = directories[0]
+        notify.data_engineering(f'Files decompressed to: "/inputs/{decompressed_directory}"')
     except Exception as err:
         notify.data_engineering("MESSAGE")
         raise err
@@ -34,8 +39,8 @@ def start(s3_object_name: str):
     # Create a local directory store using our new files
     try:
         store = LocalDirectoryStore(f"inputs/{decompressed_directory}")
+        notify.data_engineering(f'LocalDirectoryStore created using: {store.get_current_source_pathlike()}')
+        notify.data_engineering(f'LocalDirectoryStore contains files: {store.get_file_names()}')
     except Exception as err:
         notify.data_engineering("MESSAGE")
         raise err
-    
-    notify.data_engineering("WORKED")
