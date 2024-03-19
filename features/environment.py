cdd8514f71750679d6160772009295835212a7a6
Sarah Johnson <Sarah.L.Johnson@ons.gov.uk>
2024-03-19 12:11:00 +0000

    Acceptance tests working

--- features/environment.py
+++ features/environment.py
@@ -1,18 +1,72 @@
-from behave import fixture
-from zipfile import ZipFile
 from pathlib import Path
-import os
+import uuid
+import time
+
+import requests
+import docker
+from docker import DockerClient
+
 
 def before_all(context):
     """
-    Before running the tests, check if the fixtures path exists
-    and if it doesn't, extract the fixtures zipfile to it.
+    Before running the tests we set up a docker container representing a fake backend.
+
+    This is a simple flask app that logs out
+    the details of requests received - this is enough for us
+    to confirm that a request is routing as required via the feature
+    files.
     """
-    fixture_destination_path = Path("tests/fixtures/data")
-    zip_path = Path("tests/fixtures/data-fixtures.zip")
+    docker_client: DockerClient = docker.from_env()
+    repo_root = "/".join(str(Path(__file__).absolute()).split("/")[:-2])
 
-    if not fixture_destination_path.exists():
-        os.mkdir(fixture_destination_path)
+    backend_dir = repo_root + "/features/docker/fake_backend"
 
-        with ZipFile(zip_path, 'r') as fixtures_zip:
-            fixtures_zip.extractall(path = fixture_destination_path)
+    # Backend service
+    docker_client.images.build(
+        path=backend_dir, nocache=True, tag="fake_backend:latest"
+    )
+    context.backend_container = docker_client.containers.run(
+        "fake_backend:latest",
+        name="fake_backend",
+        ports={"5001": "5001"},
+        publish_all_ports=True,
+        network_mode="bridge",
+        detach=True,
+    )
+    container_info = docker_client.containers.get("fake_backend")
+    context.backend_ip = container_info.attrs["NetworkSettings"]["Networks"]["bridge"][
+        "IPAddress"
+    ]
+    time.sleep(10)
+
+
+def before_scenario(context, scenario):
+    """
+    Test setup that runs at the start of each individual scenario.
+    """
+
+    # Stick a uuid in the docker logs for all services,
+    # so we can demarcate (split) the docker logs later
+    # to check the things we expect should have been
+    # logged out actually have been.
+    context.request_id = str(uuid.uuid4())
+    requests.get(
+        f"http://127.0.0.1:5001/request-ids/{context.request_id}", data=scenario.name
+    )
+
+    # By default requests sets some (entirely sensible)
+    # headers on all requests which can lead to some
+    # unexpected results in the context of testing.
+    # So we're going to always start each scenario with
+    # a custom session with all such defaults removed.
+    context.session = requests.Session()
+    context.session.headers = {}
+
+
+def after_all(context):
+    """
+    Stop and remove the docker container after all tests have finished running.
+    """
+    context.backend_container.stop()
+    context.backend_container.remove()
+    ...
---
+++ features/steps/temporary.py
@@ -0,0 +1,126 @@
+import json
+from behave import Given, Then
+
+
+def _parse_destination_url_from_log(log: str) -> str:
+    """
+    Get the receiving path from the docker log
+    """
+    path_line = [x for x in log.split("\n") if "this-requests-url" in x]
+    assert len(path_line) == 1, f'Cannot find "this-requests-url" in service log {log}'
+    return "/" + path_line[0].split("/")[-1].strip()
+
+
+def _parse_request_headers_as_dict_from_log(log: str) -> dict:
+    """
+    Get the headers from the docker logs as a dictionary.
+    """
+    assert "this-requests-headers:" in log, (
+        "'this-requests-headers:' expected but not found in: " f"{log}"
+    )
+    headers_dict_as_str = (
+        log.split("this-requests-headers:")[1].replace("'", '"').split("}")[0] + "}"
+    )
+    headers_as_dict = json.loads(headers_dict_as_str)
+    return headers_as_dict
+
+
+def get_request_logs(container, request_id) -> str:
+    """
+    Get docker logs for this request as one big string.
+    """
+    # Get logs relevant to this specific request
+    docker_logs = str(container.logs().decode("utf-8"))
+    assert (
+        request_id in docker_logs
+    ), f"""
+        Could not find: {request_id} in:
+        {str(docker_logs)}
+        """
+    return docker_logs.split(request_id)[1]
+
+
+@Given('a request json payload of "{request_json_payload}"')
+def step_impl(context, request_json_payload):
+    """
+    Load a JSON payload file to submit as part of the request.
+    """
+    with open(request_json_payload, "r") as f:
+        context.json = json.load(f)
+
+
+@Given("a request with the headers")
+def step_impl(context):
+    """
+    Set request headers from table in scenario.
+    """
+    headers = {}
+    for row in context.table:
+        key = row["key"].strip()
+        value = row["value"].strip()
+        headers[key] = value
+    context.request_headers = headers
+
+
+@Given('I send the request to the upload service at "{upload_service_endpoint_url}"')
+def step_impl(context, upload_service_endpoint_url):
+    """
+    Submit a GET request to the specified endpoint.
+    """
+    context.request_path = upload_service_endpoint_url
+    context.response = context.session.get(
+        f"http://127.0.0.1:5001{upload_service_endpoint_url}",
+        headers=context.request_headers,
+        json=context.json,
+    )
+    context.receiving_service_log = get_request_logs(
+        context.backend_container, context.request_id
+    )
+
+
+@Then('the backend receives a request to "{upload_service_endpoint}"')
+def step_impl(context, upload_service_endpoint):
+    """
+    Check that the path in question appears in the logs
+    of the receiving service.
+    """
+    destination_url = _parse_destination_url_from_log(context.receiving_service_log)
+
+    assert (
+        upload_service_endpoint in destination_url
+    ), f"""
+        Cannot find path "{upload_service_endpoint}" in destination url: {destination_url}.
+        """
+
+
+@Then('the json payload received should match "{request_json_payload}"')
+def step_impl(context, request_json_payload):
+    """
+    Ensure that the request body contains the JSON data specified in the test file.
+    """
+    request_body = context.response.request.body.decode("utf-8")
+    with open(request_json_payload, "r") as f:
+        data = json.load(f)
+    assert data == json.loads(request_body)
+
+
+@Then("the headers received should match")
+def step_impl(context):
+    """
+    Use the 'this-requests-headers' text from the
+    docker logs to pull our one json entries worth of headers and
+    parse it back into a python dictionary.
+    """
+    headers_as_dict = _parse_request_headers_as_dict_from_log(
+        context.receiving_service_log
+    )
+
+    # Check that the headers dict contains the expected key-value pairs
+    for row in context.table:
+        key = row["key"].strip()
+        value = row["value"].strip()
+
+        assert key in headers_as_dict, f'No "{key}" header found in {headers_as_dict}'
+        assert (
+            value in headers_as_dict[key]
+        ), f'Expecting header "{key}" to have value "{value}, but got "{headers_as_dict[key]}"'
