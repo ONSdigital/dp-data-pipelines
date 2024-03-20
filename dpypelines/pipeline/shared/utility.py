f92b4ef328c830c5cdab56861a565585c7563cb5
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-03-20 10:10:09 +0000

    adressed comments

--- dpypelines/pipeline/shared/utility.py
+++ dpypelines/pipeline/shared/utility.py
@@ -5,10 +5,10 @@ def enrich_online(message_creating_function):
         msg: str = message_creating_function(*args)
         
         #Get the enviormentl variable
-        enrich_message = os.environ.get('ENRICH_MESSAGE')
+        enrich_message = os.environ.get('ENRICH_OUTGOING_MESSAGES')
         #If there is a value stored add (enrich) message otherwise return original message
         if enrich_message is not None:
-            return msg + " " + enrich_message + "\n notification-source"
+            return msg + enrich_message + "\n notification-source: ENRICH_OUTGOING_MESSAGES"
         else:
              return msg
         
