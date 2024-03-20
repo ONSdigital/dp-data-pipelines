b8b7f4d3391a7c132837a9114347ef8793f08481
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-03-20 14:14:33 +0000

    changed the test

--- tests/pipelines/pipeline/shared/test_utility.py
+++ tests/pipelines/pipeline/shared/test_utility.py
@@ -1,38 +1,26 @@
 import pytest
+from _pytest.monkeypatch import monkeypatch
 import os
 
 from dpypelines.pipeline.shared.utility import enrich_online
 from dpypelines.pipeline.shared.message import unexpected_error
 
 
-@enrich_online
-def very_much_expected_error(msg: str, error: Exception) -> str:
-    """
-    We've caught an unexpected error. Make a sensible message explaining
-    the problem.
-    """
-    error_type = error.__class__.__name__
-    message = f"""
-        {msg}
 
-        Error type: {error_type}
-        Error: {error}
+def test_enrich_decorator(monkeypatch):
+    """
+    Test we can control the behaviour of the enrich online decorator via an env var.
     """
 
-    return message
-
-
-def test_enrich_message():
-    os.environ["ENRICH_MESSAGE"] = "Please work"
-    
-
-    error = Exception("Some file is missing.")
-    expected_message = """
-        Something went wrong
-
-        Error type: Exception
-        Error: Some file is missing.
-    """
-    expected_message = expected_message + " " + "Please work" + "\n notification-source"
-
-    assert very_much_expected_error("Something went wrong", error) == expected_message
+    @enrich_online
+    def _foo_func() -> str:
+          return "foo"
+          
+     # no env var set
+    assert _foo_func() == "foo"
+     
+     # creating an enviormental variable for the test
+    monkeypatch.setenv("ENRICH_OUTGOING_MESSAGES", "boo")
+     
+     # env var is set
+    assert _foo_func() == "fooboo"
