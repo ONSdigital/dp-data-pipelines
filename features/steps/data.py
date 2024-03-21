247ab1637736cf1c9c3664a019d458b3907237ef
Sarah Johnson <Sarah.L.Johnson@ons.gov.uk>
2024-03-21 10:42:30 +0000

    fixed file naming and creation of fixtures

--- features/steps/data.py
+++ features/steps/data.py
@@ -6,7 +6,7 @@ import shutil
 from tempfile import TemporaryDirectory
 
 
-@given(u'a temporary source directory of files')
+@given("a temporary source directory of files")
 def step_impl(context):
     files_to_retrieve = {}
 
@@ -15,36 +15,37 @@ def step_impl(context):
     temporary_dir.mkdir()
 
     try:
-        
+
         for row in context.table:
             # Populate a dictionary with the table contents from the context.
             file = row["file"].strip()
             fixture = row["fixture"].strip()
-            files_to_retrieve[file] = fixture
+            files_to_retrieve[fixture] = file
 
             assert files_to_retrieve, f"No fixtures found to match input dictionary."
 
         fixtures_path = Path("features/fixtures/data")
 
         for root, dirs, files in os.walk(fixtures_path):
-            for file in files: 
-                if file in files_to_retrieve.values():
+            for file in files:
+                if file in files_to_retrieve.keys():
                     fixture_path_data = fixtures_path / file
 
                     with open(fixture_path_data) as f:
                         file_data = f.read()
 
-                    save_path = temporary_dir / file
+                    save_path = temporary_dir / files_to_retrieve[file]
                     with open(save_path, "w") as f:
                         f.write(file_data)
-            
+
         context.temporary_dir = temporary_dir
-    
+
     # If anything goes wrong, make sure the temporary directory gets removed.
     except Exception:
         shutil.rmtree(temporary_dir)
 
-@given(u'v1_data_ingress starts using the temporary source directory')
+
+@given("v1_data_ingress starts using the temporary source directory")
 def step_impl(context):
 
     try:
