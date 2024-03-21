dd088eb294cef9a0a1c05891a51fadf06ad6e8ba
mike <mikelovesbooks@gmail.com>
2024-03-21 20:24:47 +0000

    tidy up

--- dpypelines/pipeline/shared/utility.py
+++ dpypelines/pipeline/shared/utility.py
@@ -1,8 +1,8 @@
 import os
 
 def enrich_online(message_creating_function):
-    def wrapper(**kwargs):
-        msg: str = message_creating_function(**kwargs)
+    def wrapper(*args, **kwargs):
+        msg: str = message_creating_function(*args, **kwargs)
         
         #Get the enviormentl variable
         enrich_message = os.environ.get('ENRICH_OUTGOING_MESSAGES', None)
