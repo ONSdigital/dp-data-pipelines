acc327a9142986e69ceaa7b83a81f79049b02011
osamede20 <osamede.asemota@ons.gov.uk>
2024-03-25 12:42:27 +0000

    first pass at unit-test for tidy csv

--- tests/pipelines/pipeline/shared/transforms/sdmx/test_sdmx_2_0.py
+++ tests/pipelines/pipeline/shared/transforms/sdmx/test_sdmx_2_0.py
@@ -1,12 +1,11 @@
 import pytest
 import pandas as pd
 
-# from dpypelines.pipeline.shared.transforms.sdmx.sdmx_2_0 import xmlToCsvSDMX2_0
-from sdmx_2_0 import xmlToCsvSDMX2_0 
+from dpypelines.pipeline.shared.transforms.sdmx.sdmx_2_0 import xmlToCsvSDMX2_0
 
-
-
-def test_xmlToCsvSDMX2_0(input_path, output_path):
+def test_xmlToCsvSDMX2_0():
+    input_path = 'dpypelines/pipeline/shared/transforms/sdmx/SUT T1500 - NATP.ESA10.SU_SDMX Output_BlueBook_25_Jan_2024_SDMX2.0_edited.xml'
+    output_path = 'dpypelines/pipeline/shared/transforms/sdmx/tidy.csv'
     complete_table =  xmlToCsvSDMX2_0(input_path, output_path)
 
     df_csv = pd.read_csv(output_path)
@@ -18,15 +17,19 @@ def test_xmlToCsvSDMX2_0(input_path, output_path):
     assert df_csv.shape[1] == complete_table.shape[1]
     assert (df_csv.iloc[0:1, :19]).equals(complete_table.iloc[0:1, :19])
     
-def test_xmlToCsvSDMX2_0_assert_errors(input_path, output_path):
-    
+
+def test_xmlToCsvSDMX2_0_assertion_errors():
     with pytest.raises(AssertionError) as err:
-        xmlToCsvSDMX2_0(input_path, output_path)
-    assert "Length of columns (or shape of column) in tidy csv doesn't match that of input xml", err
+        input_path = 'dpypelines/pipeline/shared/transforms/sdmx/SUT T1500 - NATP.ESA10.SU_SDMX Output_BlueBook_25_Jan_2024_SDMX2.0_edited.xml'
+        output_path = 'dpypelines/pipeline/shared/transforms/sdmx/tidy.csv'
 
-# input_path = 'SUT T1500 - NATP.ESA10.SU_SDMX Output_BlueBook_25_Jan_2024 (SDMX 2.0)edited.xml',
-# output_path = 'tidy.csv'
-test_xmlToCsvSDMX2_0('SUT T1500 - NATP.ESA10.SU_SDMX Output_BlueBook_25_Jan_2024 (SDMX 2.0)edited.xml','tidy.csv')
-
-
-
+        complete_table =  xmlToCsvSDMX2_0(input_path, output_path)
+        df_csv = pd.read_csv(output_path)
+        df_csv.replace({'Test': {
+            False: "false"
+        }}, inplace=True)
+        assert len(df_csv.columns.values.tolist()) == len(complete_table.columns.values.tolist())
+        assert sorted(df_csv.columns.values.tolist()) == sorted(complete_table.columns.values.tolist())
+        assert df_csv.shape[1] == complete_table.shape[1]
+        assert (df_csv.iloc[0:1, :19]).equals(complete_table.iloc[0:1, :19])
+    assert f"Number (or shape) of columns in tidy csv does not match expected number (or shape) of columns" in str(err.value)
