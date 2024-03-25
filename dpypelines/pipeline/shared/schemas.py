f3632eb1b5640bb3e3041880c5ff1c00f61c1e79
muazzam <muazzam.chaudhary@gsscogs.uk>
2024-03-25 11:25:47 +0000

    trying to get a verified commit

--- dpypelines/pipeline/shared/schemas.py
+++ dpypelines/pipeline/shared/schemas.py
@@ -7,6 +7,7 @@ def get_config_schema_path(config: dict) -> Path:
     """
     Returns the local path to a schema from the `$id` specified in a pipeline config dictionary
     """
+    
     # Getting the absolute path to the 'dpypelines/' directory
     dpypelines_absolute_path = Path(__file__).parent.parent.parent.absolute()
 
