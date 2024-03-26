aa57449a1f8ad65e004778d3562a12b35209cd3b
Sarah Johnson <Sarah.L.Johnson@ons.gov.uk>
2024-03-26 15:02:39 +0000

    fixed test

--- tests/pipelines/pipeline/shared/test_utility.py
+++ tests/pipelines/pipeline/shared/test_utility.py
@@ -6,7 +6,6 @@ from dpypelines.pipeline.shared.utility import enrich_online
 from dpypelines.pipeline.shared.message import unexpected_error
 
 
-
 def test_enrich_decorator(monkeypatch):
     """
     Test we can control the behaviour of the enrich online decorator via an env var.
@@ -14,13 +13,13 @@ def test_enrich_decorator(monkeypatch):
 
     @enrich_online
     def _foo_func() -> str:
-          return "foo"
-          
-     # no env var set
+        return "foo"
+
+    # no env var set
     assert _foo_func() == "foo"
-     
-     # creating an enviormental variable for the test
+
+    # creating an enviormental variable for the test
     monkeypatch.setenv("ENRICH_OUTGOING_MESSAGES", "boo")
-     
-     # env var is set
-    assert _foo_func() == "foo" + "/nboo"
+
+    # env var is set
+    assert _foo_func() == "foo" + " boo"
