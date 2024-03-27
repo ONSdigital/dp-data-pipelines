65595f7f29db596ad78fd39882c225d97b3345b2
mike <mikelovesbooks@gmail.com>
2024-03-27 07:08:49 +0000

    enrich success/fail messages

--- dpypelines/pipeline/shared/notification.py
+++ dpypelines/pipeline/shared/notification.py
@@ -11,8 +11,16 @@ class PipelineMessenger(SlackMessenger):
         ), "Unable to find required environment variable to populate webhook_url argument"
         super().__init__(self.webhook_url)
 
+
     def failure(self):
+        enrich_message = os.environ.get("ENRICH_OUTGOING_MESSAGES", None)
+        if enrich_message is not None:
+            self.msg_str(f":boom: {enrich_message}")
         self.msg_str(":boom:")
 
     def success(self):
+        enrich_message = os.environ.get("ENRICH_OUTGOING_MESSAGES", None)
+        if enrich_message is not None:
+            self.msg_str(f":white_check_mark: {enrich_message}")
         self.msg_str(":white_check_mark:")
+
