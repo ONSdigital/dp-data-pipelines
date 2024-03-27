73b59f37bb81e484af5e421f30e06d3555bd7721
James Bryant <jamesbryant792@gmail.com>
2024-03-27 16:53:26 +0000

    further validation and tests added

--- dpypelines/pipeline/shared/transforms/validate_transform.py
+++ dpypelines/pipeline/shared/transforms/validate_transform.py
@@ -17,13 +17,20 @@ def check_header_info(header):
     for key in header.keys():
         assert key in expected_keys, f"{key} is not expected"
     
-def check_tables_list(tables_list):
+def check_tables_list(file, tables_list):
     # check the tables list is a list of dicts
-    # each dict is the same size - will this always be the case
     assert type(tables_list) == list, f"expected 'tables' variable to be a list but got {type(tables_list)}" 
     for item in tables_list:
         assert type(item) == dict, f"expected item of list to be a dict but got {type(item)}"
         assert 'na_:Obs' in item.keys(), "no observation data found in section of the 'tables' variable"
+        for obs_type in item['na_:Obs']:
+            assert '@TIME_PERIOD' in obs_type.keys(), "@TIME_PERIOD not found"
+            assert '@OBS_VALUE' in obs_type.keys(), "@OBS_VALUE not found"
+            assert '@OBS_STATUS' in obs_type.keys(), "@OBS_STATUS not found"
+            assert '@CONF_STATUS' in obs_type.keys(), "@CONF_STATUS not found"
+        
+    number_of_series = get_number_of_series(file)
+    assert len(tables_list) == number_of_series, "series length from file does not match number of tables"
 
 def check_temp_df(df):
     # check that temp_df is not empty and is as expected
@@ -33,4 +40,23 @@ def check_xml_type(data_As_dict):
     # check that the xml type is 'CompactData'
     assert len(data_As_dict.keys()) == 1, "xml format looks incorrect"
     assert 'CompactData' in data_As_dict.keys(), "could not find 'CompactData' in xml data"
+    
+def check_read_in_sdmx(file):
+    # check sdmx can be read in
+    with open(file, 'r') as file:
+        xml_content = file.read()
+    assert xml_content.startswith('<?xml'), "file does not appear to be xml"
+
+def get_number_of_series(file):
+    # returns number of series from xml
+    with open(file) as f:
+        number_of_series = sum(1 for line in f if '<na_:Series' in line)   
+    assert number_of_series != 0, "did not find any series in xml"     
+    return number_of_series
+
+def check_tidy_data_columns(df_columns):
+    # check tidy data headers have no “@” or “na:”
+    for col in df_columns:
+        assert "@" not in col, "@ found in tidy data column name"
+        assert "na:" not in col, "na: found in tidy data column name"
     
--- tests/pipelines/pipeline/shared/transforms/test_validate_transform.py
+++ tests/pipelines/pipeline/shared/transforms/test_validate_transform.py
@@ -9,6 +9,9 @@ from dpypelines.pipeline.shared.transforms.validate_transform import (
     check_tables_list,
     check_temp_df,
     check_xml_type,
+    check_read_in_sdmx,
+    get_number_of_series,
+    check_tidy_data_columns
 )
 
 test_dir = Path(__file__).parents[4]
@@ -59,29 +62,56 @@ def test_check_header_info_incorrect_header():
     assert f"{incorrect_header} is not expected" in str(err.value)
     
 def test_check_tables_list_incorrect_input():
+    fixture_file = Path(fixtures_files_dir / 'test_validate_transform.xml')
     tables = {'key': ''}
     
     with pytest.raises(AssertionError) as err:
-        check_tables_list(tables)
+        check_tables_list(fixture_file, tables)
         
     assert f"expected 'tables' variable to be a list but got {type(tables)}" in str(err.value)
     
 def test_check_tables_list_incorrect_key_value():
+    fixture_file = Path(fixtures_files_dir / 'test_validate_transform.xml')
     item = ''
     tables = [item]
     
     with pytest.raises(AssertionError) as err:
-        check_tables_list(tables)
+        check_tables_list(fixture_file, tables)
         
     assert f"expected item of list to be a dict but got {type(item)}" in str(err.value)
     
 def test_check_tables_list_missing_obs():
+    fixture_file = Path(fixtures_files_dir / 'test_validate_transform.xml')
     tables = [{'key1': '', 'key2': ''}]
     
     with pytest.raises(AssertionError) as err:
-        check_tables_list(tables)
+        check_tables_list(fixture_file, tables)
         
     assert "no observation data found in section of the 'tables' variable" in str(err.value)
+
+def test_check_tables_list_missing_time_period():
+    fixture_file = Path(fixtures_files_dir / 'test_validate_transform.xml')
+    tables = [{'key1': '', 'na_:Obs': [{'@OBS_VALUE': '0', '@OBS_STATUS': 'A', '@CONF_STATUS': 'F'}]}]
+    
+    with pytest.raises(AssertionError) as err:
+        check_tables_list(fixture_file, tables)
+        
+    assert "@TIME_PERIOD not found" in str(err.value)
+    
+def test_check_tables_list_different_number_of_series():
+    fixture_file = Path(fixtures_files_dir / 'test_validate_transform.xml')
+    tables = [{
+        'key1': '', 
+        'na_:Obs': [{'@TIME_PERIOD': '', '@OBS_VALUE': '', '@OBS_STATUS': '', '@CONF_STATUS': ''}],
+        }, {
+        'key1': '', 
+        'na_:Obs': [{'@TIME_PERIOD': '', '@OBS_VALUE': '', '@OBS_STATUS': '', '@CONF_STATUS': ''}],
+        }]
+    
+    with pytest.raises(AssertionError) as err:
+        check_tables_list(fixture_file, tables)
+        
+    assert "series length from file does not match number of tables" in str(err.value)
         
 def test_check_temp_df_empty_df():
     df = pd.DataFrame()
@@ -90,4 +120,44 @@ def test_check_temp_df_empty_df():
         check_temp_df(df)
         
     assert f"temp_df is returning an empty dataframe" in str(err.value)
+    
+def test_check_xml_type_incorrect_dict_size():
+    data = {'key1': '', 'key2': ''}
+    
+    with pytest.raises(AssertionError) as err:
+        check_xml_type(data)
+        
+    assert "xml format looks incorrect" in str(err.value)
+    
+def test_check_xml_type_incorrect_xml_type():
+    data = {'GeneralData': ''}
+    
+    with pytest.raises(AssertionError) as err:
+        check_xml_type(data)
+        
+    assert "could not find 'CompactData' in xml data" in str(err.value)
+
+def test_check_read_in_sdmx_incorrect_file():
+    fixture_file = Path(fixtures_files_dir / 'test_validate_csv_data.csv') # any file that isn't an xml
+    
+    with pytest.raises(AssertionError) as err:
+        check_read_in_sdmx(fixture_file)
+        
+    assert "file does not appear to be xml" in str(err.value)
+    
+def test_get_number_of_series_zero_series_found():
+    fixture_file = Path(fixtures_files_dir / 'test_validate_csv_data.csv') # any file that isn't an xml
+    
+    with pytest.raises(AssertionError) as err:
+        get_number_of_series(fixture_file)
+        
+    assert "did not find any series in xml" in str(err.value)
+    
+def test_check_tidy_data_columns_incorrect_columns():
+    df_columns = ["@column"]
+    
+    with pytest.raises(AssertionError) as err:
+        check_tidy_data_columns(df_columns)
+        
+    assert "@ found in tidy data column name" in str(err.value)
     
