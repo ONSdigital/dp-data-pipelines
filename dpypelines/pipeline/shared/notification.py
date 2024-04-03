444e371fb75d06b8f3c53d1533d103e77af8f9e8
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-04-03 14:10:22 +0100

    initial change

--- dpypelines/pipeline/shared/notification.py
+++ dpypelines/pipeline/shared/notification.py
@@ -3,13 +3,19 @@ import os
 from dpytools.slack.slack import SlackMessenger
 
 
+isDisabled = os.environ.get("DISABLE_NOTIFICATIONS", 'False').lower()
+
 class PipelineMessenger(SlackMessenger):
     def __init__(self, webhook_url):
         self.webhook_url = webhook_url
-        assert (
-            self.webhook_url is not None
-        ), "Unable to find required environment variable to populate webhook_url argument"
-        super().__init__(self.webhook_url)
+        #check for the variable, if its not set or set to flase act as previously
+        if not isDisabled:
+            assert (
+                self.webhook_url is not None
+            ), "Unable to find required environment variable to populate webhook_url argument"
+            super().__init__(self.webhook_url)
+        else:
+            super().__init__(self)
 
     def failure(self):
         self.msg_str(":boom:")
