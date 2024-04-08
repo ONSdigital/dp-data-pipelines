0cb8167a16f45ad55a5166566d7818d5e50ed15c
bailed22 <david.bailey@ons.gov.uk>
2024-04-08 16:23:50 +0100

    first test commit

---
+++ dpypelines/pipeline/shared/transforms/sanity_check.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+
+
+def sdmx_sanity_check_v1(sdmx_file: Path):
+    """
+    Sanity check that the recieved sdmx file is actually an sdmx file.
+    """
+    assert sdmx_file.name.endswith(".xml"), "Invalid sdmx_file"
+
+    try:
+        pd.read_xml(sdmx_file, nrows=10)
+    except Exception as err:
+        raise Exception(f"Failed to read in xml - {sdmx_file}") from err
