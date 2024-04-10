8505818b2fd176c908a33f2478f388572a841b19
bailed22 <david.bailey@ons.gov.uk>
2024-04-10 14:11:01 +0100

    adding fucntion to the script

--- dpypelines/pipeline/shared/transforms/sanity_check.py
+++ dpypelines/pipeline/shared/transforms/sanity_check.py
@@ -11,6 +11,17 @@ def sdmx_sanity_check_v1(sdmx_file: Path):
     assert sdmx_file.name.endswith(".xml"), "Invalid sdmx_file"
 
     try:
-        pd.read_xml(sdmx_file, nrows=10)
+        pd.read_xml(sdmx_file)
     except Exception as err:
         raise Exception(f"Failed to read in xml - {sdmx_file}") from err
+    
+file = '/Users/davidbailey/Downloads/SUT T1500 - NATP.ESA10.SU_SDMX Output_BlueBook_25_Jan_2024 (SDMX 2.0).xml'
+sdmx_sanity_check_v1(Path(file))
+
+
+
+
+
+
+
+
