7b872172cebb4d496397bb8c3730d21580b90821
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-04-10 14:50:14 +0100

    n

--- dpypelines/s3_tar_received.py
+++ dpypelines/s3_tar_received.py
@@ -26,10 +26,10 @@ def start(s3_object_name: str):
 
     # Decompress the tar file to the workspace
     try:
-        raise Exception("force an exception for testing")
-        #raise ValueError("Force an error to appear")
+        #raise Exception("force an exception for testing")
+        raise ValueError("Force an error to appear")
         decompress_s3_tar(s3_object_name, "input")
-    except Exception as err:
+    except ValueError as err:
         de_messenger.failure()
         raise ValueError("Failed to decompress tar file",
             message.unexpected_error(
