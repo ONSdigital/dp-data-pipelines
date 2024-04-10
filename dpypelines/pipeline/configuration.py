a29e9c8092108f744d7e2425d87264b88a363382
Sarah Johnson <Sarah.L.Johnson@ons.gov.uk>
2024-04-10 10:38:02 +0100

    PR comments addressed

--- dpypelines/pipeline/configuration.py
+++ dpypelines/pipeline/configuration.py
@@ -7,8 +7,8 @@ from dpypelines.pipeline.shared.transforms.sdmx.v1 import (
 # Set a regex pattern matching the `dataset_id` as `CONFIGURATION` dictionary key
 # All fields are required in order for a pipeline transform to run successfully
 CONFIGURATION = {
-    # Default configuration (regex pattern matches any string of characters of length >= 0)
-    "^.*$": {
+    # This is an example of how to set configuration details for a specific dataset_id (`cpih` in this case)
+    "^cpih$": {
         "config_version": 1,
         "transform": sdmx_compact_2_0_prototype_1,
         "transform_inputs": {"^data.xml$": sdmx_sanity_check_v1},
@@ -17,8 +17,9 @@ CONFIGURATION = {
         "supplementary_distributions": ["^data.xml$"],
         "secondary_function": dataset_ingress_v1,
     },
-    # `cpih` config details
-    "^cpih$": {
+    # Default configuration (regex pattern matches any string of characters of length >= 0)
+    # This *always* needs to be the final entry in the dictionary to prevent inadvertent matches
+    "^.*$": {
         "config_version": 1,
         "transform": sdmx_compact_2_0_prototype_1,
         "transform_inputs": {"^data.xml$": sdmx_sanity_check_v1},
