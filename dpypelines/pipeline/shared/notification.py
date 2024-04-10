a428146ff8e3eb06200bbc6a4ad606508023e2d6
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-04-10 08:59:29 +0100

    added pipeline messenger

--- dpypelines/pipeline/shared/notification.py
+++ dpypelines/pipeline/shared/notification.py
@@ -82,3 +82,18 @@ def notifier_from_env_var_webhook(env_var: str) -> BasePipelineNotifier:
     ), f"The specified env var {env_var} is not present on this system."
 
     return PipelineNotifier(web_hook)
+
+
+class PipelineMessenger(SlackMessenger):
+    def __init__(self, webhook_url):
+        self.webhook_url = webhook_url
+        assert (
+            self.webhook_url is not None
+        ), "Unable to find required environment variable to populate webhook_url argument"
+        super().__init__(self.webhook_url)
+
+    def failure(self):
+        self.msg_str(":boom:")
+
+    def success(self):
+        self.msg_str(":white_check_mark:")
