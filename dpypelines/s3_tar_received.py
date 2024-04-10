0e58fe0d72753a533ab34951af9569259bc01387
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-04-10 13:27:36 +0100

    f

--- dpypelines/s3_tar_received.py
+++ dpypelines/s3_tar_received.py
@@ -31,7 +31,7 @@ def start(s3_object_name: str):
         decompress_s3_tar(s3_object_name, "input")
     except Exception as err:
         de_messenger.failure()
-        raise ValueError("Force an error to appear") from err
+        raise ValueError("Force an error to appear")
         #raise Exception("Failed to decompress tar file",
          #   message.unexpected_error(
           #      f"Failed to decompress tar file {s3_object_name}", err
