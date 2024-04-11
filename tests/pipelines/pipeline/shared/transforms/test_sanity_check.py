1fe18a8d0b17fb3124fa2c3ca090a9549ee53223
bailed22 <95625808+bailed22@users.noreply.github.com>
2024-04-11 16:33:38 +0100

    SDMX Transform - Sanity check SDMX file (validity) (#96)

---
+++ tests/pipelines/pipeline/shared/transforms/test_sanity_check.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pytest
+from dpypelines.pipeline.shared.transforms.sanity_check import sdmx_sanity_check_v1
+
+def test_sdmx_sanity_check_v1_file_extension():
+    with pytest.raises(AssertionError) as e:
+        sdmx_sanity_check_v1(Path("data.csv"))
+    assert "Invalid sdmx_file" in str(e.value)
+
+
+def test_sdmx_sanity_check_v1_read_xml():
+    with pytest.raises(Exception) as e:
+        sdmx_sanity_check_v1(Path("data.xml"))
+    assert "Failed to read in xml" in str(e.value)
