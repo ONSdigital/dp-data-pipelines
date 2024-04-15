4d69aab575e128308d24d1b8231d9f300a972b17
Sarah Johnson <Sarah.L.Johnson@ons.gov.uk>
2024-04-15 11:17:40 +0100

    Fixed tests

--- tests/pipelines/pipeline/test_configuration.py
+++ tests/pipelines/pipeline/test_configuration.py
@@ -2,10 +2,10 @@ from dpypelines.pipeline.configuration import get_pipeline_config
 
 
 def test_get_pipeline_config():
-    pipeline_config = get_pipeline_config("cpih")
+    pipeline_config, config_keys = get_pipeline_config("cpih")
     assert pipeline_config["config_version"] == 1
 
 
 def test_get_pipeline_config_default():
-    pipeline_config = get_pipeline_config("some-id")
+    pipeline_config, config_keys = get_pipeline_config("some-id")
     assert pipeline_config["config_version"] == 1
