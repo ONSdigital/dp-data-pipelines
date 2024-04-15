25b39c0cf17b3401915820171328dd69140e93a6
RedWalters <jonathan.j.walters@Gmail.com>
2024-04-15 15:51:31 +0100

    -.-

--- dpypelines/pipeline/shared/transforms/sdmx/compact/v20/prototype/v1.py
+++ dpypelines/pipeline/shared/transforms/sdmx/compact/v20/prototype/v1.py
@@ -1,9 +1,9 @@
 import json
 import xml.etree.ElementTree as ET
 from datetime import datetime
+
 import pandas as pd
 import xmltodict
-
 
 from dpypelines.pipeline.shared.transforms.utils import (
     convert, 
