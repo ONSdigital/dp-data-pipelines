d750643865a4564620a7a30334e5f4381097108d
RedWalters <jonathan.j.walters@Gmail.com>
2024-04-15 15:29:55 +0100

    Adding metadata creation to transform file, and adding required utils, more info on metadata here https://confluence.ons.gov.uk/pages/viewpage.action?pageId=190307047

--- dpypelines/pipeline/shared/transforms/utils.py
+++ dpypelines/pipeline/shared/transforms/utils.py
@@ -11,6 +11,13 @@ def pathify(label):
         r"-$", "", re.sub(r"-+", "-", re.sub(r"[^\w/]", "-", unidecode(label).lower()))
     )
 
+def set_key(dictionary, key, value):
+     if key not in dictionary:
+         dictionary[key] = value
+     elif type(dictionary[key]) == list:
+         dictionary[key].append(value)
+     else:
+         dictionary[key] = [dictionary[key], value]
 
 def set_key(dictionary, key, value):
     if key not in dictionary:
