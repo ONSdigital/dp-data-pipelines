23b74af86a86a49fef5b1b7d531ccdea2af1b175
RedWalters <jonathan.j.walters@Gmail.com>
2024-04-23 11:26:37 +0100

    Update v1.py

--- dpypelines/pipeline/shared/transforms/sdmx/compact/v20/prototype/v1.py
+++ dpypelines/pipeline/shared/transforms/sdmx/compact/v20/prototype/v1.py
@@ -281,7 +281,7 @@ def generate_versions_metadata(
 
     # with open(str(outputPath) + pathify(dataset_title) + "_" + str(datetime.now().strftime("%Y-%m")) + "-metadata.json", "w") as outfile:
     #    json.dump(versions_template, outfile, ensure_ascii=False, indent=4)
-    with open(output_path, "w") as outfile:
+    with open(outputPath, "w") as outfile:
         json.dump(versions_template, outfile, ensure_ascii=False, indent=4)
 
     return versions_template
