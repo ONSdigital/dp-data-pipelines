825039b119617406a475af53f911cbe075f31e54
Moasib-Arif <moasib_arif@yahoo.com>
2024-04-29 15:00:28 +0100

    Minor changes added

--- dpypelines/pipeline/dataset_ingress_v1.py
+++ dpypelines/pipeline/dataset_ingress_v1.py
@@ -46,10 +46,7 @@ def dataset_ingress_v1(files_dir: str, pipeline_config: dict):
         raise err
 
     try:
-        if os.getenv('DISABLE_EMAILS') != 'true':
-            submitter_email = get_submitter_email()
-        else:
-            submitter_email = None
+        submitter_email = get_submitter_email()
     except Exception as err:
         de_notifier.failure()
         raise err
