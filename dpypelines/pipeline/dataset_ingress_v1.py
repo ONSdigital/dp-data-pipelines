df45d132194f3a67539f38bf72c1b4bb05109bc9
Moasib-Arif <moasib_arif@yahoo.com>
2024-04-29 14:28:49 +0100

    minor changes

--- dpypelines/pipeline/dataset_ingress_v1.py
+++ dpypelines/pipeline/dataset_ingress_v1.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from dpytools.logging.logger import DpLogger
@@ -40,7 +41,10 @@ def dataset_ingress_v1(files_dir: str, pipeline_config: dict):
     )
 
     try:
-        submitter_email = get_submitter_email()
+        if os.getenv('DISABLE_EMAILS') != 'true':
+            submitter_email = get_submitter_email()
+        else:
+            submitter_email = None
     except Exception as err:
         de_notifier.failure()
         raise err
