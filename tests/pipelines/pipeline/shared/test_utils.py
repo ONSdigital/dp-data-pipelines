ca8987f3b176b9371ea10367932361a9249276b7
Sarah Johnson <Sarah.L.Johnson@ons.gov.uk>
2024-05-01 14:33:50 +0100

    PR comments addressed

--- tests/pipelines/pipeline/shared/test_utils.py
+++ tests/pipelines/pipeline/shared/test_utils.py
@@ -1,12 +1,6 @@
-import os
-
 import pytest
 
-from dpypelines.pipeline.shared.utils import (
-    get_florence_access_token,
-    get_supplementary_distribution_file,
-    str_to_bool,
-)
+from dpypelines.pipeline.shared.utils import get_florence_access_token, str_to_bool
 
 
 def test_str_to_bool_valid_values():
@@ -23,7 +17,7 @@ def test_str_to_bool_valid_values():
     assert str_to_bool("false  ") is False
 
 
-def test_str_to_bool_valid_raises_for_invlaid_str_value():
+def test_str_to_bool_valid_raises_for_invalid_str_value():
     """
     Test str_to_bool utility will raise when given a non
     expected string value
@@ -60,36 +54,6 @@ def test_florence_access_token_set_as_env_var():
 
 
 def test_florence_access_token_not_set_as_env_var():
-    mp = pytest.MonkeyPatch()
-    env_var = os.environ.get("FLORENCE_TOKEN", None)
-    if env_var is not None:
-        mp.delenv("FLORENCE_TOKEN")
-    florence_token = get_florence_access_token()
-    assert florence_token == "not-implemented"
-
-
-def test_get_supplementary_distribution_files():
-    files = ["data.xml", "data.csv", "data.xls"]
-    matching, extension = get_supplementary_distribution_file(files, "^data.xml$")
-    assert matching == "data.xml"
-    assert extension == ".xml"
-
-
-def test_get_supplementary_distribution_files_more_then_one_match():
-    files = ["data.xml", "data.csv", "data.xls", "data.xml"]
-    with pytest.raises(AssertionError) as err:
-        get_supplementary_distribution_file(files, "^data.xml$")
-    assert (
-        "Error finding file matching pattern ^data.xml$: matching files are ['data.xml', 'data.xml']"
-        in str(err.value)
-    )
-
-
-def test_get_supplementary_distribution_files_no_match():
-    files = ["data.csv", "data.xls"]
-    with pytest.raises(AssertionError) as err:
-        get_supplementary_distribution_file(files, "^data.xml$")
-    assert (
-        "Error finding file matching pattern ^data.xml$: matching files are []"
-        in str(err.value)
-    )
+    with pytest.raises(NotImplementedError) as err:
+        get_florence_access_token()
+    assert "No Florence token set" in str(err.value)
