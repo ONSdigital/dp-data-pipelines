113ebecfd226a79d485aafa6a4f8db01a4c339c5
NickPapONS <nick.papoutsakis@ons.gov.uk>
2024-05-20 11:02:07 +0100

    Remove unusued imports

--- dpypelines/pipeline/shared/transforms/sdmx/generic/v21/prototype/v1.py
+++ dpypelines/pipeline/shared/transforms/sdmx/generic/v21/prototype/v1.py
@@ -1,9 +1,6 @@
-import json
-
 import pandas as pd
 import xmltodict
 
-from dpypelines.pipeline.shared.transforms.utils import flatten_dict, pathify, set_key
 from dpypelines.pipeline.shared.transforms.validate_transform_v21 import (
     check_columns_of_dataframes_are_unique,
     check_header_info,
