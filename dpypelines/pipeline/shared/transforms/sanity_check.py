10917005b40420bd79296ac48fa5d145b3101dc6
osamede20 <osamede.asemota@ons.gov.uk>
2024-06-05 15:05:04 +0100

    corrected some logic on the codes

--- dpypelines/pipeline/shared/transforms/sanity_check.py
+++ dpypelines/pipeline/shared/transforms/sanity_check.py
@@ -1,15 +1,8 @@
 from pathlib import Path
 
-import pandas as pd
 
-
-def sdmx_sanity_check_v1(sdmx_file: Path):
-    """
-    Sanity check that the recieved sdmx file is actually an sdmx file.
-    """
-    assert sdmx_file.name.endswith(".xml"), "Invalid sdmx_file"
-
-    try:
-        pd.read_xml(sdmx_file)
-    except Exception as err:
-        raise Exception(f"Failed to read in xml - {sdmx_file}") from err
+def sdmx_sanity_check_v1(xml_file: Path):
+    # check sdmx can be read in
+    with open(xml_file, "r") as f:
+        xml_content = f.read()
+    assert xml_content.startswith("<?xml"), "file does not appear to be xml"
