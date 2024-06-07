7f3064d7ea6b6c27d00debc3e505c4864faee36d
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-06-07 08:54:24 +0100

    fix?

--- dpypelines/s3_tar_received.py
+++ dpypelines/s3_tar_received.py
@@ -1,4 +1,3 @@
-from threading import local
 from dpytools.logging.logger import DpLogger
 from dpytools.s3.basic import decompress_s3_tar
 from dpytools.stores.directory.local import LocalDirectoryStore
