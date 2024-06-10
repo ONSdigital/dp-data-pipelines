31150d262e13c5ed6dba076626cb31b89472c42f
osamede20 <osamede.asemota@ons.gov.uk>
2024-06-10 16:13:25 +0100

    major rework

--- dpypelines/pipeline/configuration.py
+++ dpypelines/pipeline/configuration.py
@@ -2,17 +2,18 @@ import re
 
 from dpypelines.pipeline.dataset_ingress_v1 import dataset_ingress_v1
 from dpypelines.pipeline.generic_file_ingress_v1 import generic_file_ingress_v1
+
+# from dpypelines.pipeline.shared.transforms.sdmx.v1 import sdmx_sanity_check_v1
 from dpypelines.pipeline.shared.transforms.sanity_check import sdmx_sanity_check_v1
 from dpypelines.pipeline.shared.transforms.sdmx.v20 import sdmx_compact_2_0_prototype_1
-from dpypelines.pipeline.shared.transforms.sdmx.v21 import (  # sdmx_sanity_check_v1,
-    sdmx_generic_2_1_prototype_1,
-)
+from dpypelines.pipeline.shared.transforms.sdmx.v21 import sdmx_generic_2_1_prototype_1
 
-# Set a regex pattern matching the `dataset_id` as `CONFIGURATION` dictionary key
+# Set a regex pattern matching the `source_id` as `CONFIGURATION` dictionary key
 # All fields are required in order for a pipeline transform to run successfully
 CONFIGURATION = {
-    # This is an example of how to set configuration details for a specific dataset_id (`cpih` in this case)
-    "^cpih$": {
+    # This is an example of how to set configuration details for a generic source_id - ending with v2_0, v2_1 for sdmx 2.0 and 2.1 respectively, and
+    # generic source_id - ending with move for an sdmx file to be moved
+    "^.*_v2_0$": {
         "config_version": 1,
         "transform": sdmx_compact_2_0_prototype_1,
         "transform_inputs": {"^data.xml$": sdmx_sanity_check_v1},
@@ -21,9 +22,7 @@ CONFIGURATION = {
         "supplementary_distributions": [{"matches": "^data.xml$", "count": "1"}],
         "secondary_function": dataset_ingress_v1,
     },
-    # Default configuration (regex pattern matches any string of characters of length >= 0)
-    # This *always* needs to be the final entry in the dictionary to prevent inadvertent matches
-    "^.*$": {
+    "^.*_v2_1$": {
         "config_version": 1,
         "transform": sdmx_generic_2_1_prototype_1,
         "transform_inputs": {"^data.xml$": sdmx_sanity_check_v1},
@@ -32,13 +31,13 @@ CONFIGURATION = {
         "supplementary_distributions": [{"matches": "^data.xml$", "count": "1"}],
         "secondary_function": dataset_ingress_v1,
     },
-    "*._move$": {
+    "^.*_move$": {
         "config_version": 1,
         "transform": None,
         "transform_inputs": {},
         "transform_kwargs": {},
         "required_files": [{"matches": "^data.xml$", "count": "1"}],
-        "supplementary_distributions": [{"matches": "^data.xml$", "count": "1"}],
+        "supplementary_distributions": {},
         "secondary_function": generic_file_ingress_v1,
     },
 }
--- dpypelines/pipeline/generic_file_ingress_v1.py
+++ dpypelines/pipeline/generic_file_ingress_v1.py
@@ -1,22 +1,16 @@
 import os
-import re
 
 from dpytools.http.upload import UploadServiceClient
 from dpytools.logging.logger import DpLogger
 from dpytools.stores.directory.local import LocalDirectoryStore
 
-from dpypelines.pipeline.shared.email_template_message import (
-    file_not_found_email,
-    submission_processed_email,
-    supplementary_distribution_not_found_email,
-)
+from dpypelines.pipeline.shared.email_template_message import file_not_found_email
 from dpypelines.pipeline.shared.notification import (
     BasePipelineNotifier,
     notifier_from_env_var_webhook,
 )
 from dpypelines.pipeline.shared.pipelineconfig.matching import (
     get_required_files_patterns,
-    get_supplementary_distribution_patterns,
 )
 from dpypelines.pipeline.shared.utils import (
     get_email_client,
@@ -65,19 +59,6 @@ def generic_file_ingress_v1(files_dir: str, pipeline_config: dict):
         logger.info("Created email client", data={"email_client": email_client})
     except Exception as err:
         logger.error("Error occurred when creating email client", err)
-        de_notifier.failure()
-        raise err
-
-    # just throw out an email to see if it works
-    try:
-        email_content = submission_processed_email()
-        email_client.send(submitter_email, email_content.subject, email_content.message)
-        logger.info(
-            "Email sent",
-            data={"submitter_email": submitter_email, "email_content": email_content},
-        )
-    except Exception as err:
-        logger.error("Error occurred when sending email", err)
         de_notifier.failure()
         raise err
 
@@ -164,64 +145,6 @@ def generic_file_ingress_v1(files_dir: str, pipeline_config: dict):
             de_notifier.failure()
             raise err
 
-    # Extract the patterns for supplementary distributions from the pipeline configuration
-    try:
-        supp_dist_patterns = get_supplementary_distribution_patterns(pipeline_config)
-        logger.info(
-            "Supplementary distribution patterns retrieved from pipeline config",
-            data={"supplementary_distribution_patterns": supp_dist_patterns},
-        )
-    except Exception as err:
-        files_in_directory = local_store.get_file_names()
-        logger.error(
-            "Error occurred when getting supplementary distribution patterns",
-            err,
-            data={"pipeline_config": pipeline_config},
-        )
-        de_notifier.failure()
-        raise err
-
-    # Check for the existence of each supplementary distribution
-    for supp_dist_pattern in supp_dist_patterns:
-        try:
-            if not local_store.has_lone_file_matching(supp_dist_pattern):
-                # Catch a trivial raise as we need the stack trace of the
-                # error for the logger, so it needs to be a raised error.
-                try:
-                    raise FileNotFoundError(
-                        f"No file found matching pattern {supp_dist_pattern}"
-                    )
-                except FileNotFoundError as err:
-                    email_content = supplementary_distribution_not_found_email(
-                        supp_dist_pattern
-                    )
-                    email_client.send(
-                        submitter_email, email_content.subject, email_content.message
-                    )
-                    logger.error(
-                        "Email sent to submitter about missing supplementary distribution file",
-                        err,
-                        data={
-                            "submitter_email": submitter_email,
-                            "supplementary_distribution_pattern": supp_dist_pattern,
-                        },
-                    )
-                    de_notifier.failure()
-                    raise err
-        except Exception as err:
-            logger.error(
-                "Error occurred when looking for supplementary distribution",
-                err,
-                data={
-                    "supplementary_distribution": supp_dist_pattern,
-                    "supplementary_distribution_patterns": supp_dist_patterns,
-                    "files_in_directory": files_in_directory,
-                    "pipeline_config": pipeline_config,
-                },
-            )
-            de_notifier.failure()
-            raise err
-
     # Allow DE's to skip the upload to s3 part of the pipeline while
     # developing code locally.
     skip_data_upload = os.environ.get("SKIP_DATA_UPLOAD", False)
@@ -247,7 +170,7 @@ def generic_file_ingress_v1(files_dir: str, pipeline_config: dict):
         # Upload output files to Upload Service
         try:
             # Create UploadClient from upload_url
-            upload_client = UploadServiceClient(upload_url)
+            UploadServiceClient(upload_url)
             logger.info(
                 "UploadClient created from upload_url", data={"upload_url": upload_url}
             )
@@ -257,59 +180,5 @@ def generic_file_ingress_v1(files_dir: str, pipeline_config: dict):
             )
             de_notifier.failure()
             raise err
-
-        # Check for supplementary distributions to upload
-        if supp_dist_patterns:
-            # Get list of all files in local store
-            all_files = local_store.get_file_names()
-            logger.info("Got all files in local store", data={"files": all_files})
-            for supp_dist_pattern in supp_dist_patterns:
-                # Get supplementary distribution filename matching pattern from local store
-                supp_dist_matching_files = [
-                    f for f in all_files if re.search(supp_dist_pattern, f)
-                ]
-                assert (
-                    len(supp_dist_matching_files) == 1
-                ), f"Error finding file matching pattern {supp_dist_pattern}: matching files are {supp_dist_matching_files}"
-
-                # Create a directory to save supplementary distribution
-                if not os.path.exists("supplementary_distributions"):
-                    os.mkdir("supplementary_distributions")
-                supp_dist_path = local_store.save_lone_file_matching(
-                    supp_dist_pattern, "supplementary_distributions"
-                )
-                logger.info(
-                    "Got supplementary distribution",
-                    data={
-                        "supplementary_distribution": supp_dist_path,
-                        "file_extension": supp_dist_path.suffix,
-                    },
-                )
-                # If the supplementary distribution is an XML file, upload to the Upload Service
-                if supp_dist_path.suffix == ".xml":
-                    try:
-                        upload_client.upload_new_sdmx(supp_dist_path)
-                        logger.info(
-                            "Uploaded supplementary distribution",
-                            data={
-                                "supplementary_distribution": supp_dist_path,
-                                "upload_url": upload_url,
-                            },
-                        )
-                    except Exception as err:
-                        logger.error(
-                            "Error uploading SDMX file to Upload Service",
-                            err,
-                            data={
-                                "supplementary_distribution": supp_dist_path,
-                                "upload_url": upload_url,
-                            },
-                        )
-                        de_notifier.failure()
-                        raise err
-                else:
-                    raise NotImplementedError(
-                        f"Uploading files of type {supp_dist_path.suffix} not supported."
-                    )
 
     de_notifier.success()
