1b34ad5ec045f8ddc82f14d9fcc372dada23fbad
osamede20 <osamede.asemota@ons.gov.uk>
2024-06-18 15:15:00 +0100

    formatted file

--- dpypelines/pipeline/shared/transforms/sanity_check.py
+++ dpypelines/pipeline/shared/transforms/sanity_check.py
@@ -21,20 +21,3 @@ def sdmx_sanity_check_v1(sdmx_file: Path):
         assert sdmx_file.name.endswith(".xml"), "Invalid sdmx_file"
     except Exception as err:
         raise Exception(f"Failed to read in xml - {sdmx_file}") from err
-
-
-# def sdmx_sanity_check_v1(sdmx_file: Path):
-#     """
-#     Sanity check that the recieved sdmx file is actually an sdmx file.
-#     """
-#     assert sdmx_file.name.endswith(".xml"), "Invalid sdmx_file"
-
-#     # with open(sdmx_file, "r") as f:
-#     #     xml_content = f.read()
-#     # assert xml_content.startswith("<?xml"), "Failed to read in xml"
-
-#     # def sdmx_sanity_check_v1(xml_file: Path):
-#     # # check sdmx can be read in
-#     #     with open(xml_file, "r") as f:
-#     #         xml_content = f.read()
-#     #     assert xml_content.startswith("<?xml"), "file does not appear to be xml"
