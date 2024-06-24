9634a1b88591a6f6ae1e220ad7b24d4e364f84e3
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-06-24 11:04:59 +0100

    trying stuff

--- testing.py
+++ testing.py
@@ -1,3 +1,28 @@
-from dpypelines import s3_tar_received
+import json
+import requests
+from requests import Response # check im correct about this import path
+from dpytools.logging.logger import DpLogger
 
-s3_tar_received.start('joes-dev-bucket-delete-later/config-no-options.tar')
+logger = DpLogger("data-ingress-pipeline")
+
+def get_scheme(url: str) -> str:
+
+    index = url.find('/')
+    scheme = url[0:index-1]
+    return scheme
+
+def get_host(response: Response) ->str:
+    json_form = response.json()
+
+    host = response.headers["Host"]
+    return host
+
+
+r: Response = requests.get("https://www.google.com")
+
+r_dict = {"method": r.request.method, "scheme": get_scheme(r.url), "host": get_host(r), "port": 443, "path": r.request.path_url, "status_code" : r.status_code,"started_at": r.headers["Date"]}
+# so given theres a requests.Response object, you could potentially have a pattern of say
+
+logger.info("Some log message involving https", response=r)
+
+
