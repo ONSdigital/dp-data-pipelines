0a7ed2e17190b5a0639cabfb34ff7c7125deb390
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-06-26 09:30:38 +0100

    f

--- dpypelines/pipeline/shared/utils.py
+++ dpypelines/pipeline/shared/utils.py
@@ -2,6 +2,7 @@
 # package is being depreciate from the standard
 # library in python >3.12
 import os
+
 from dpytools.email.ses.client import SesClient
 from dpytools.utilities.utilities import str_to_bool
 from email_validator import EmailNotValidError, validate_email
