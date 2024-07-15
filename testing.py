4245b1a71c946f38af060de6488b1d7c24003d33
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-07-15 13:32:27 +0100

    fix

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ import subprocess
 
 def get_commit_ID():
     try:
-        commit_id=subprocess.check_output(["git", "log", "-1", "--format%H"]).strip().decode("utf-8")
+        commit_id=subprocess.check_output(["git", "log", "-1", "--format=%H"]).strip().decode("utf-8")
         return commit_id
     except subprocess.CalledProcessError as err:
         print("Error while fetching commit ID", err)
