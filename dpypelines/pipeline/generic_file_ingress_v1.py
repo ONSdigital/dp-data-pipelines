211f61d91794ae2b8a308cde1296c241aa8c1895
Jozsef K <krajcsirik20@gmail.com>
2024-10-03 13:04:47 +0100

    f

--- dpypelines/pipeline/generic_file_ingress_v1.py
+++ dpypelines/pipeline/generic_file_ingress_v1.py
@@ -81,11 +81,14 @@ def generic_file_ingress_v1(files_dir: str, pipeline_config: dict):
 
     # Get Upload Service URL from environment variable
     try:
-        upload_url = os.environ.get("UPLOAD_SERVICE_URL", None)
-        assert (
-            upload_url is not None and os.environ["SKIP_DATA_UPLOAD"] is False
-        ), "UPLOAD_SERVICE_URL environment variable not set."
-        logger.info("Got Upload Service URL", data={"upload_url": upload_url})
+        if os.environ["SKIP_DATA_UPLOAD"] is False:
+            upload_url = os.environ.get("UPLOAD_SERVICE_URL", None)
+            assert (
+                upload_url is not None
+            ), "UPLOAD_SERVICE_URL environment variable not set."
+            logger.info("Got Upload Service URL", data={"upload_url": upload_url})
+        else:
+            upload_url = os.environ.get("UPLOAD_SERVICE_URL", None)
     except Exception as err:
         logger.error("Error occurred when getting Upload Service URL", err)
         notifier.failure()
