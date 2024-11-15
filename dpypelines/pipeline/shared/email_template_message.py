64fcd9bf77c4dc00354feb9389b184474d04efb5
NickPapONS <nick.papoutsakis@ons.gov.uk>
2024-11-15 10:05:27 +0000

    Template functions

--- dpypelines/pipeline/shared/email_template_message.py
+++ dpypelines/pipeline/shared/email_template_message.py
@@ -32,3 +32,39 @@ def submission_processed_email() -> EmailContent:
     subject = "Data Submission: Processed Successfully"
     message = "Your data submission has been processed. If there were any issues, you will receive a separate email with the details."
     return EmailContent(subject, message)
+
+
+def successful_dataset_upload_email(dataset_id: str) -> EmailContent:
+    """
+    Create a subject and message to confirm a successful dataset upload.
+    """
+    subject = "Dataset Upload: Completed Successfully"
+    message = f"The uploaded dataset {dataset_id} has been loaded successfully."
+    return EmailContent(subject, message)
+
+
+def failed_dataset_upload_email(dataset_id: str, error_info: str) -> EmailContent:
+    """
+    Create a subject and message to report a dataset upload failure.
+    """
+    subject = "Dataset Upload: Failed"
+    message = f"The upload for dataset {dataset_id} has failed. Error details: {error_info}"
+    return EmailContent(subject, message)
+
+
+def successful_validation_email(dataset_id: str) -> EmailContent:
+    """
+    Create a subject and message to confirm a dataset passing validation successfully.
+    """
+    subject = "Dataset Ingest: Validation Successful"
+    message = f"The validation for dataset {dataset_id} has passed successfully."
+    return EmailContent(subject, message)
+
+
+def failed_validation_email(dataset_id: str, validation_info) -> EmailContent:
+    """
+    Create a subject and message to report failed validation on dataset ingest.
+    """
+    subject = "Dataset Ingest: Validation Failed"
+    message = f"The validation for dataset {dataset_id} has failed. Failure details: {validation_info}"
+    return EmailContent(subject, message)
