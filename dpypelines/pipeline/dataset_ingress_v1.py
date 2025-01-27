13605712860a906ca23529af6853296abc79c7a8
Jozsef K <krajcsirik20@gmail.com>
2025-01-27 11:40:44 +0000

    f

--- dpypelines/pipeline/dataset_ingress_v1.py
+++ dpypelines/pipeline/dataset_ingress_v1.py
@@ -16,7 +16,9 @@ from dpypelines.pipeline.shared.email_templates import (
     supplementary_distribution_not_found_email,
 )
 from dpypelines.pipeline.shared.pipelineconfig.matching import get_matching_pattern
-from dpypelines.pipeline.shared.transforms.process_tranform_module import process_transform
+from dpypelines.pipeline.shared.transforms.process_tranform_module import (
+    process_transform,
+)
 from dpypelines.pipeline.shared.utils import (
     get_email_client,
     get_mimetype,
@@ -188,7 +190,7 @@ def dataset_ingress_v1(files_dir: str, pipeline_config: dict):
 
     csv_path, metadat_path = process_transform(
         local_store, pipeline_config, files_in_directory, de_notifier
-        )
+    )
     # TODO - validate the metadata once we have a schema for it.
 
     # TODO - validate the csv once we know what we're validating
