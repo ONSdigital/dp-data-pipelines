dfa8ac7bf3fdc544354f4af045b517fc85378dbc
Jozsef K <krajcsirik20@gmail.com>
2025-01-28 14:19:23 +0000

    f

---
+++ myscript.py
@@ -0,0 +1,24 @@
+from dpypelines.pipeline.dataset_ingress_v1 import dataset_ingress_v1
+
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+
+config = {
+        "config_version": 1,
+        "transform": None,
+        "transform_inputs": {},
+        "transform_kwargs": {},
+        "required_files": [
+            {"matches": "^data.csv$"},
+            {"matches": "^metadata.json$"},
+        ],
+        "supplementary_distributions": {},
+        "secondary_function": dataset_ingress_v1,
+    }
+
+
+data_path = "data/"
+
+resuolt = dataset_ingress_v1(data_path, config)
