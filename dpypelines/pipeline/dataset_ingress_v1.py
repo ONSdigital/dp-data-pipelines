1a299ea8a2e9e9d23a61e8146a72e81c6a893b37
Jozsef K <krajcsirik20@gmail.com>
2025-01-29 09:10:47 +0000

    fix

--- dpypelines/pipeline/dataset_ingress_v1.py
+++ dpypelines/pipeline/dataset_ingress_v1.py
@@ -8,7 +8,6 @@ from dpytools.stores.directory.local import LocalDirectoryStore
 from dpytools.utilities.utilities import str_to_bool
 
 from dpypelines.pipeline.shared.email_templates import (
-    failed_file_upload_email,
     submission_processed_email,
     successful_file_upload_email,
     successful_validation_email,
