5081f2066a0356bf65fe209297b7e193bc3d944f
emmaons <emma.baldachin@ext.ons.gov.uk>
2025-02-11 11:18:26 +0000

    added some comments on test_dataset_ingress for Moa

--- tests/pipelines/pipeline/test_dataset_ingress.py
+++ tests/pipelines/pipeline/test_dataset_ingress.py
@@ -6,7 +6,11 @@ from dpypelines.pipeline.dataset_ingress_v1 import dataset_ingress_v1
 
 test_cases_base_dir = Path("tests/fixtures/test-cases/dataset_ingress_v1")
 
-
+# This currently tests data ingress FROM the point of having files that exist in a local directory
+# However, as we are pulling everything into one ETL module, the starting point is going to be DOWNLOADING from S3 (when Joe's work is merged)
+# A test path for test data in S3 is needed as an argument for this.
+# Check with the team if a folder of valid test data already exists in s3- otherwise make one!
+# TODO: can delete this comment chunk
 def test_dataset_ingress_v1():
     """
     Tests that `dataset_ingress_v1()` returns True if valid data, metadata and manifest files are provided.
