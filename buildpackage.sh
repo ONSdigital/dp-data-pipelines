183055a476eb9ab6f1e1658799dcf75688a4df18
NickPapONS <nick.papoutsakis@ons.gov.uk>
2025-02-17 10:50:55 +0000

    Converted to bash

---
+++ buildpackage.sh
@@ -0,0 +1,10 @@
+# Constants for AWS commands
+s3Bucket="dp-sandbox-whl-package-upload"
+wheelPath="dist/dpypelines-0.1.0-py3-none-any.whl"
+wheelKey="dpypelines-0.1.0-py3-none-any.whl"
+
+# Build package with poetry. Default output is dist/
+poetry build
+
+# Put object to S3 bucket
+aws s3api put-object --bucket $s3Bucket --body $wheelPath --key $wheelKey
