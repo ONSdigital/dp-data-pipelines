dddd77c0d442ae79fd24c4c806b65e1da967261e
NickPapONS <nick.papoutsakis@ons.gov.uk>
2025-03-05 10:49:37 +0000

    fix: fixing import

--- dpypelines/pipeline/messages/error_handler_module.py
+++ dpypelines/pipeline/messages/error_handler_module.py
@@ -3,7 +3,7 @@ from typing import Optional
 from dpytools.logging.logger import DpLogger
 
 from dpypelines.pipeline.messages.utils import get_email_client
-from dpypelines.pipeline.messages.utils import get_notifier
+from dpypelines.pipeline.utils import get_notifier
 
 logger = DpLogger("data-ingress-pipelines")
 
