48526cd9964a7c2b8e5e27cd65825145d031e23b
Jozsef K <krajcsirik20@gmail.com>
2025-03-27 15:35:44 +0000

    updated lock

--- wip-integration-test.py
+++ wip-integration-test.py
@@ -1,4 +1,32 @@
-from dpypelines import s3_folder_received
+import boto3
+import json
+from botocore.exceptions import ClientError
 
 
-s3_folder_received.start("dp-sandbox-ingest-submission-bucket/input/e2e.zip")
+def get_secret(value: str) -> str:
+
+    secret_name = "dp-sandbox-secrets"
+    region_name = "eu-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    data = json.loads(secret)
+    return data[value]
+
+
+print(get_secret("DE_SLACK_WEBHOOK"))
