ca5259de0ba88029eaf5b3d565db0e56aae5b0dd
Moasib-Arif <moasib.arif@ons.gov.uk>
2025-04-08 15:57:59 +0100

    Minor changes

--- dpypelines/pipeline/messages/email_templates.py
+++ dpypelines/pipeline/messages/email_templates.py
@@ -87,6 +87,7 @@ def failed_metadata_submission(dataset_id: str, error_info: str) -> EmailContent
     message = f"The metadata for {dataset_id} could not be submitted to the Dataset API. Submission failure details: {error_info}"
     return EmailContent(subject, message)
 
+
 def test():
     """
     Test release
