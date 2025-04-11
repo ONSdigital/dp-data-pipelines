ba38a05efb5438bb0337ed9d33d21e8fe4e626a4
Jim Washbrook <jim.washbrook@ext.ons.gov.uk>
2025-04-11 14:18:12 +0100

    fix: add code back

--- .github/workflows/release-script.js
+++ .github/workflows/release-script.js
@@ -5,6 +5,24 @@ const CHANGELOG_PATH = "./CHANGELOG.md";
 
 const getFileAsUTF8 = (filePath) =>  fs.readFileSync(filePath, 'utf8');
 
+const getLatestChangelog = () => {
+  const changelog = getFileAsUTF8(CHANGELOG_PATH);
+  if (!changelog || typeof changelog !== 'string') {
+    throw new Error(`Changelog falsy or not a string; changelog is ${changelog}`);
+  }
+
+  const versionSections = changelog.split(/^## v/m);
+  const startIndex = versionSections[0].trim().toUpperCase().startsWith("# CHANGELOG") ? 1 : 0;
+  
+  if (startIndex >= versionSections.length) {
+    throw new Error(`Changelog startIndex invalid. startIndex was ${startIndex} and versionSections is ${versionSections}. Changelog: \n\n${changelog}`);
+  }
+  
+  const latestSection = versionSections[startIndex];
+  
+  return '## ' + latestSection.trim();
+} 
+
 class GitHubReleaseManager {
   constructor(github, context) {
     this.github = github;
