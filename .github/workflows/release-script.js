a731f7c0ece4cd87aacc0b3ee52e8bb3c821dbf1
Jim Washbrook <jim.washbrook@ext.ons.gov.uk>
2025-04-11 13:57:53 +0100

    fix: changelog parsing

--- .github/workflows/release-script.js
+++ .github/workflows/release-script.js
@@ -1,8 +1,27 @@
 const fs = require('fs');
 const path = require('path'); // Added missing import
 
+const CHANGELOG_PATH = "./CHANGELOG.md";
 
 const getFileAsUTF8 = (filePath) =>  fs.readFileSync(filePath, 'utf8');
+
+const getLatestChangelog = () => {
+  const changelog = getFileAsUTF8(CHANGELOG_PATH);
+  if (!changelog || typeof changelog !== 'string') {
+    throw new Error(`Changelog falsy or not a string; changelog is ${changelog}`);
+  }
+
+  const versionSections = changelog.split(/^## v/m);
+  const startIndex = versionSections[0].trim().toUpperCase().startsWith("# CHANGELOG") ? 1 : 0;
+  
+  if (startIndex >= versionSections.length) {
+    throw new Error(`Changelog startIndex invalid. startIndex was ${startIndex} and versionSections is ${versionSections}. Changelog: \n\n${changelog}`);
+  }
+  
+  const latestSection = versionSections[startIndex];
+  
+  return '## ' + latestSection.trim();
+} 
 
 class GitHubReleaseManager {
   constructor(github, context) {
@@ -103,7 +122,7 @@ class GitHubReleaseManager {
   }
 
   async createRelease(tag) {
-    const releaseNotes = getFileAsUTF8("./CHANGELOG.md");
+    const releaseNotes = getLatestChangelog();
 
     return this.github.rest.repos.createRelease({
       owner: this.owner,
