eef5c325ef1c1301ee53029d0d98050d4e7e72ff
Red Walters <jonathan.j.walters@gmail.com>
2025-04-24 13:18:14 +0100

    adding logs to folder recieved

--- dpypelines/s3_folder_received.py
+++ dpypelines/s3_folder_received.py
@@ -1,4 +1,5 @@
 from dpytools.logging.logger import DpLogger
+from datetime import datetime
 
 from dpypelines.pipeline.config import JobConfiguration
 from dpypelines.pipeline.messages.error_handler_module import error_handler
@@ -33,6 +34,9 @@ def start(s3_object_name: str, *args, **kwargs):
         **kwargs: Optional extra keyword arguments.
     """
     try:
+
+        start_time = datetime.now().isoformat()
+
         # Set up clients.
         notifier, email_client = setup_clients()
 
@@ -75,6 +79,18 @@ def start(s3_object_name: str, *args, **kwargs):
                 delete_s3_processing_folder(
                     s3_object_name, decompressed_file_dir, s3_processing_folder
                 )
+
+                end_time = datetime.now().isoformat()
+
+                try:
+                    logger.info("Performance Metrics", data = {
+                        "Pipeline Start" : start_time,
+                        "Pipeline End" : end_time,
+                        "Pipeline ID" : manifest_dict['source_id']
+                    })
+                except Exception as log_err:
+                    logger.error("Performance logging failed", log_err)
+
                 return True
             else:
                 # Copy all files in S3 "processing" folder to S3 "dataset-type-not-static" folder
