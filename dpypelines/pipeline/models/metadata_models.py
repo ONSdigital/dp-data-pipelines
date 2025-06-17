86d93eb51e442ceef36e1fe4e433078696feb5e7
Jim Washbrook <jim.washbrook@ext.ons.gov.uk>
2025-06-17 12:00:39 +0100

    fix: map quality desigination + edition

--- dpypelines/pipeline/models/metadata_models.py
+++ dpypelines/pipeline/models/metadata_models.py
@@ -1,6 +1,8 @@
-from typing import List, Optional
+from enum import Enum
+from optparse import Option
+from typing import Annotated, List, Optional
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import AfterValidator, BaseModel, EmailStr, Field
 
 from dpypelines.pipeline.messages.utils import get_mimetype
 
@@ -31,9 +33,26 @@ class SubmissionContact(BaseModel):
     email: EmailStr
 
 
+class QualityDesignation(str, Enum):
+    AccreditedOfficial = "accredited-official"
+    Official = "official"
+    OfficialInDevelopment = "official-in-development"
+
+
+def map_quality_desigination(value: Optional[str]) -> QualityDesignation:
+    if value is None:
+        return QualityDesignation.AccreditedOfficial
+    try:
+        return QualityDesignation(value)
+    except ValueError:
+        return QualityDesignation.AccreditedOfficial
+    
+def quality_desigination_checker(value: Optional[str]) -> str:
+    return map_quality_desigination(value).value
+        
 class DatasetVersion(BaseModel):
-    edition_title: Optional[str] = None
-    quality_designation: Optional[str] = None
+    edition_title: Optional[str] = Field(alias="edition")
+    quality_designation: Annotated[Optional[str], AfterValidator(quality_desigination_checker)]
     usage_notes: Optional[List[UsageNote]] = Field(default_factory=list)
     alerts: Optional[List[Alert]] = Field(default_factory=list)
     distributions: List[Distribution]
